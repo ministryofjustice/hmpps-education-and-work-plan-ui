ac15966992ef531f4da07dd2859cfe27cf72d2e7
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2023-06-28 18:57:30 +0100

    Cypress tests now do basic accessibility tests (#6)

--- integration_tests/pages/page.ts
+++ integration_tests/pages/page.ts
@@ -5,12 +5,36 @@ export default abstract class Page {
     return new constructor()
   }
 
-  constructor(private readonly title: string) {
+  constructor(
+    private readonly title: string,
+    private readonly options: { axeTest?: boolean } = {
+      axeTest: true,
+    },
+  ) {
     this.checkOnPage()
+    this.checkCsfrTokenForFormBasedPages()
+    if (options.axeTest) {
+      this.runAxe()
+    }
   }
 
   checkOnPage(): void {
     cy.get('h1').contains(this.title)
+  }
+
+  checkCsfrTokenForFormBasedPages = (): void => {
+    cy.get('body').then(body => {
+      body.find('form').each((idx, form) => {
+        cy.wrap(form).find('input[name=_csrf]').should('not.have.value', '')
+      })
+    })
+  }
+
+  runAxe = (): void => {
+    cy.injectAxe()
+    cy.checkA11y(null, {
+      includedImpacts: ['critical', 'serious'],
+    })
   }
 
   signOut = (): PageElement => cy.get('[data-qa=signOut]')
