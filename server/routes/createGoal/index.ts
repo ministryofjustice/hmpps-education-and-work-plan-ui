766fe689047db6e30e98bddd63ccbeaf0adc9039
Matt Wills <matt.wills@digital.justice.gov.uk>
2023-06-30 11:13:08 +0100

    RR-02 Add createGoalController.ts

---
+++ server/routes/createGoal/index.ts
@@ -0,0 +1,13 @@
+import { RequestHandler, Router } from 'express'
+import { Services } from '../../services'
+import CreateGoalController from './createGoalController'
+import asyncMiddleware from '../../middleware/asyncMiddleware'
+
+export default (router: Router, services: Services) => {
+  const createGoalController = new CreateGoalController()
+  const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+
+  get('/plan/:prisonNumber/goals/create', createGoalController.getCreateGoalView)
+  get('/plan/:prisonNumber/goals/add-step', createGoalController.getAddStepView)
+  get('/plan/:prisonNumber/goals/add-note', createGoalController.getAddNoteView)
+}
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,9 +2,9 @@ import { type RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
+import createGoal from './createGoal'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function routes(service: Services): Router {
+export default function routes(services: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
@@ -12,17 +12,7 @@ export default function routes(service: Services): Router {
     res.render('pages/index')
   })
 
-  get('/plan/:prisonNumber/goals/create', (req, res, next) => {
-    res.render('pages/goal/create/index')
-  })
-
-  get('/plan/:prisonNumber/goals/add-step', (req, res, next) => {
-    res.render('pages/goal/add-step/index')
-  })
-
-  get('/plan/:prisonNumber/goals/add-note', (req, res, next) => {
-    res.render('pages/goal/add-note/index')
-  })
+  createGoal(router, services)
 
   return router
 }
