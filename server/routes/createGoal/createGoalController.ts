a09c888ceb5b350c6c6059bd47c05ef756dd210c
Matt Wills <matt.wills@digital.justice.gov.uk>
2023-06-30 13:45:49 +0100

    RR-02 Fix lint issue

--- server/routes/createGoal/createGoalController.ts
+++ server/routes/createGoal/createGoalController.ts
@@ -1,7 +1,23 @@
 import type { RequestHandler } from 'express'
+import type { Prisoner } from 'prisonRegisterApiClient'
+import createError from 'http-errors'
+import logger from '../../../logger'
+import PrisonerSearchService from '../../services/prisonerSearchService'
 
 export default class CreateGoalController {
+  constructor(private readonly prisonerSearchService: PrisonerSearchService) {}
+
   getCreateGoalView: RequestHandler = async (req, res, next): Promise<void> => {
+    const { prisonNumber } = req.params
+    let prisoner: Prisoner
+    try {
+      prisoner = await this.prisonerSearchService.getPrisonerByPrisonNumber(prisonNumber)
+    } catch (error) {
+      next(createError(404, 'Prisoner not found'))
+      return
+    }
+    logger.info(prisoner)
+
     res.render('pages/goal/create/index')
   }
 
