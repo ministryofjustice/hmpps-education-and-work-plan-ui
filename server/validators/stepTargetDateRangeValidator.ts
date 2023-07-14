c0bd7340c990a882caff18fbc8bfe5af98bf5bc7
mattwills-moj-digital <135589132+mattwills-moj-digital@users.noreply.github.com>
2023-07-14 15:10:45 +0100

    RR-81 Fix to targetDateRange error message (#44)

--- server/validators/stepTargetDateRangeValidator.ts
+++ server/validators/stepTargetDateRangeValidator.ts
@@ -1,14 +1,8 @@
-const MAX_TARGET_DATE_RANGE_LENGTH = 50
-
 export default function validateStepTargetDateRange(targetDateRange?: string): Array<string> {
   const errors: Array<string> = []
 
-  if (!targetDateRange) {
-    errors.push('Select a target date range')
-  } else if (targetDateRange.length < 1) {
-    errors.push('Select a target date range')
-  } else if (targetDateRange.length > MAX_TARGET_DATE_RANGE_LENGTH) {
-    errors.push(`The target date range must be ${MAX_TARGET_DATE_RANGE_LENGTH} characters or less`)
+  if (!targetDateRange || targetDateRange.length < 1) {
+    errors.push('Please select when they will achieve this by')
   }
 
   return errors
