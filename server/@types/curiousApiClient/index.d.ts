d02b9b3421ecf91bd41202769d08aa2199b15587
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2023-07-17 11:05:51 +0100

    RR-107 - Basic config and types for calling curious-api (#46)

---
+++ server/@types/curiousApiClient/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'curiousApiClient' {
+  import { components } from '../curiousApi'
+
+  export type LearnerEducation = components['schemas']['LearnerEducationDTO']
+  export type LearnerNeurodivergence = components['schemas']['LearnerNeurodivergenceDTO']
+}
