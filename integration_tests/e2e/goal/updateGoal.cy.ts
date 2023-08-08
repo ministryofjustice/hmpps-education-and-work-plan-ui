707c57f2b8dd1ad94f89b0960ba78eb244eff3cc
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2023-08-08 17:51:24 +0100

    RR-32 - Cypress tests (#98)

--- integration_tests/e2e/goal/updateGoal.cy.ts
+++ integration_tests/e2e/goal/updateGoal.cy.ts
@@ -2,6 +2,7 @@ import Page from '../../pages/page'
 import UpdateGoalPage from '../../pages/goal/UpdateGoalPage'
 import OverviewPage from '../../pages/overview/OverviewPage'
 import AuthorisationErrorPage from '../../pages/authorisationError'
+import { UpdateGoalRequest } from '../../mockApis/educationAndWorkPlanApi'
 
 context('Update a goal', () => {
   beforeEach(() => {
@@ -68,6 +69,42 @@ context('Update a goal', () => {
 
     // Then
     Page.verifyOnPage(OverviewPage)
+  })
+
+  it('should be able to add a new step as part of updating a goal', () => {
+    // Given
+    const prisonNumber = 'G6115VJ'
+    cy.signIn()
+
+    cy.visit(`/plan/${prisonNumber}/view/overview`)
+    const overviewPage = Page.verifyOnPage(OverviewPage)
+
+    const updateGoalPage = overviewPage.clickUpdateButtonForFirstGoal()
+
+    // When
+    updateGoalPage //
+      // The goal loaded from the wiremock stub data has 2 steps. After adding another step, the new step will be referenced as step 3
+      .addAnotherStep()
+      .setStepTitle(3, 'A brand new step')
+      .setStepStatus(3, 'Started')
+      .setStepTargetDateRange(3, '6 to 12 months')
+      .submitPage()
+
+    // Then
+    Page.verifyOnPage(OverviewPage)
+    // Assert that the expected new step was sent in the UpdateGoal request to the API - TODO, there has to be a better way of doing this
+    cy.task<UpdateGoalRequest>('getUpdateGoalRequestBody')
+      .then(updateGoalRequestBody => {
+        const newStep = { ...updateGoalRequestBody.steps.slice(-1)[0] }
+        return newStep
+      })
+      .should('deep.equal', {
+        stepReference: '',
+        sequenceNumber: '3',
+        title: 'A brand new step',
+        status: 'ACTIVE',
+        targetDateRange: 'SIX_TO_TWELVE_MONTHS',
+      })
   })
 
   it('should redirect to auth-error page given user does not have any authorities', () => {
--- integration_tests/mockApis/educationAndWorkPlanApi.ts
+++ integration_tests/mockApis/educationAndWorkPlanApi.ts
@@ -1,5 +1,5 @@
 import { SuperAgentRequest } from 'superagent'
-import { stubFor } from './wiremock'
+import { getMatchingRequests, stubFor } from './wiremock'
 import actionPlans from '../mockData/actionPlanByPrisonNumberData'
 
 const createGoal = (): SuperAgentRequest =>
@@ -28,6 +28,18 @@ const updateGoal = (
       status: 204,
       headers: { 'Content-Type': 'application/json;charset=UTF-8' },
     },
+  })
+
+const getUpdateGoalRequestBody = (
+  prisonNumber = 'G6115VJ',
+  goalReference = '10efc562-be8f-4675-9283-9ede0c19dade',
+): Promise<UpdateGoalRequest> =>
+  getMatchingRequests({
+    method: 'PUT',
+    urlPattern: `/action-plans/${prisonNumber}/goals/${goalReference}`,
+  }).then(data => {
+    const { requests } = JSON.parse(data.text)
+    return JSON.parse(requests[0].body)
   })
 
 const getActionPlanForPrisonerWithNoGoals = (prisonNumber = 'G6115VJ'): SuperAgentRequest =>
@@ -69,7 +81,25 @@ export default {
   createGoal,
   getActionPlan,
   updateGoal,
+  getUpdateGoalRequestBody,
 
   getActionPlanForPrisonerWithNoGoals,
   getActionPlan500Error,
+}
+
+export interface UpdateGoalRequest {
+  goalReference: string
+  title: string
+  status: string
+  steps: [
+    {
+      stepReference?: string
+      title: string
+      status: string
+      targetDateRange: string
+      sequenceNumber: number
+    },
+  ]
+  reviewDate?: string
+  notes?: string
 }
