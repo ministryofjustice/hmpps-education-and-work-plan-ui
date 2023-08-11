7c4a51c929d6a763e7cc96ec4636d763217b25c4
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-08-11 10:17:34 +0100

    use a better name for formatting the target date range value

---
+++ server/filters/formatTargetDateRangeValue.ts
@@ -0,0 +1,11 @@
+export default function formatTargetDateRangeValue(value: string): string {
+  const dateRangeValue = DateRangeValue[value as keyof typeof DateRangeValue]
+  return dateRangeValue
+}
+
+export enum DateRangeValue {
+  ZERO_TO_THREE_MONTHS = '0 to 3 months',
+  THREE_TO_SIX_MONTHS = '3 to 6 months',
+  SIX_TO_TWELVE_MONTHS = '6 to 12 months',
+  MORE_THAN_TWELVE_MONTHS = 'More than 12 months',
+}
