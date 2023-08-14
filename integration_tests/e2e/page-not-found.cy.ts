806f32bb6e5e5094f57192751dae54ef902406e6
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-08-14 10:07:36 +0100

    add cypress tests for 404 pages for authorised and unauthorised users (#106)
    * add cypress tests for 404 pages for authorised and unauthorised users
    
    * add missing stubs to run beforeEach
    
    * use a page that has the authentication required in Cypress test

---
+++ integration_tests/e2e/page-not-found.cy.ts
@@ -0,0 +1,38 @@
+import Page from '../pages/page'
+import Error404Page from '../pages/error404'
+import AuthorisationErrorPage from '../pages/authorisationError'
+
+context('404 Page Not Found', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubAuthUser')
+    cy.task('stubSignInAsUserWithViewAuthority')
+  })
+
+  it('should redirect to auth-error page given user does not have any authorities', () => {
+    // Given
+    cy.task('stubSignIn')
+    cy.signIn()
+
+    const prisonNumber = 'G6115VJ'
+
+    // When
+    cy.visit(`/plan/${prisonNumber}/view/overview/unknown`, { failOnStatusCode: false })
+
+    // Then
+    Page.verifyOnPage(AuthorisationErrorPage)
+  })
+
+  it('should redirect the 404 page when an authenticated user navigates to a non-existent page', () => {
+    // Given
+    cy.signIn()
+
+    const prisonNumber = 'G6115VJ'
+
+    // When
+    cy.visit(`/plan/${prisonNumber}/view/overview/unknown`, { failOnStatusCode: false })
+
+    // Then
+    Page.verifyOnPage(Error404Page)
+  })
+})
