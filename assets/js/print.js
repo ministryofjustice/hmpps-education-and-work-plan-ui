ffad3d54395a6f39d7ad6f0ffe2803151576063e
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-08-24 11:23:22 +0100

    check the print link exists before executing and add code comments

--- assets/js/print.js
+++ assets/js/print.js
@@ -1,12 +1,24 @@
+/**
+ * Expand all the Overview notes details elements on print
+ */
 function showDetailsElementOnPrint() {
   document.querySelectorAll('.app-notes-expander').forEach(details => {
     details.setAttribute('open', '')
   })
 }
 
+window.addEventListener('beforeprint', () => showDetailsElementOnPrint())
+
+/**
+ * Handle the print page link event
+ */
+var printLink = document.getElementById('print-link')
+
 function showPrintWindow() {
   window.print()
 }
 
-window.addEventListener('beforeprint', () => showDetailsElementOnPrint())
-document.getElementById('print-link').addEventListener('click', () => showPrintWindow())
+// Check that the print link exists before executing the event listener
+if (printLink) {
+  printLink.addEventListener('click', () => showPrintWindow())
+}
