92f6504ddb5421d168f0bf2e987b360094588ced
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-08-31 13:31:42 +0100

    RR-33 check for deleting steps before the validation check

--- server/routes/updateGoal/updateGoalController.ts
+++ server/routes/updateGoal/updateGoalController.ts
@@ -43,23 +43,6 @@ export default class UpdateGoalController {
     const updateGoalForm: UpdateGoalForm = { ...req.body }
     req.session.updateGoalForm = updateGoalForm
 
-    const errors = validateUpdateGoalForm(updateGoalForm)
-    if (errors.length > 0 && !updateGoalForm.action.includes('delete-step-')) {
-      req.flash('errors', errors)
-      return res.redirect(`/plan/${prisonNumber}/goals/${goalReference}/update`)
-    }
-
-    // Redirect to the desired page based on the form action
-    if (updateGoalForm.action === 'add-another-step') {
-      // Initialize a new UpdateStepForm with the next step number
-      const currentHighestStepNumber = Math.max(...updateGoalForm.steps.map(step => step.stepNumber))
-      const nextStepNumber = currentHighestStepNumber + 1
-      const newStep: UpdateStepForm = { stepNumber: nextStepNumber, status: 'NOT_STARTED' }
-      updateGoalForm.steps.push(newStep)
-      // Redirect back to the Update Goal page with named anchor taking the user straight to the new step
-      return res.redirect(`/plan/${prisonNumber}/goals/${goalReference}/update#steps[${nextStepNumber - 1}][title]`)
-    }
-
     // Remove the desired step on the action delete step
     if (updateGoalForm.action.startsWith('delete-step-')) {
       // Get the step index inbetween the 2 characters [ ] from the action value
@@ -75,6 +58,24 @@ export default class UpdateGoalController {
       // Redirect back to the Update Goal page with named anchor taking the user to the edit and remove steps section
       return res.redirect(`/plan/${prisonNumber}/goals/${goalReference}/update#edit-and-remove-steps`)
     }
+
+    const errors = validateUpdateGoalForm(updateGoalForm)
+    if (errors.length > 0) {
+      req.flash('errors', errors)
+      return res.redirect(`/plan/${prisonNumber}/goals/${goalReference}/update`)
+    }
+
+    // Redirect to the desired page based on the form action
+    if (updateGoalForm.action === 'add-another-step') {
+      // Initialize a new UpdateStepForm with the next step number
+      const currentHighestStepNumber = Math.max(...updateGoalForm.steps.map(step => step.stepNumber))
+      const nextStepNumber = currentHighestStepNumber + 1
+      const newStep: UpdateStepForm = { stepNumber: nextStepNumber, status: 'NOT_STARTED' }
+      updateGoalForm.steps.push(newStep)
+      // Redirect back to the Update Goal page with named anchor taking the user straight to the new step
+      return res.redirect(`/plan/${prisonNumber}/goals/${goalReference}/update#steps[${nextStepNumber - 1}][title]`)
+    }
+
     return res.redirect(`/plan/${prisonNumber}/goals/${goalReference}/update/review`)
   }
 
