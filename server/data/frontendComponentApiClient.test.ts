28973f61e6c6fe6323b1e93706513b26f2280195
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-09-12 11:46:25 +0100

    RR-296 add tests for the frontend components API client

---
+++ server/data/frontendComponentApiClient.test.ts
@@ -0,0 +1,36 @@
+import nock from 'nock'
+import config from '../config'
+import FrontendComponentApiClient from './frontendComponentApiClient'
+
+describe('frontendComponentApiClient', () => {
+  const frontendComponentApiClient = new FrontendComponentApiClient()
+
+  config.apis.frontendComponents.url = 'http://localhost:8200'
+  let fakeFrontendComponentApi: nock.Scope
+
+  beforeEach(() => {
+    fakeFrontendComponentApi = nock(config.apis.frontendComponents.url)
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  describe('getComponent', () => {
+    it('should return data from api', async () => {
+      // Given
+      const userToken = 'a-user-token'
+
+      const expectedResponse = { data: 'data' }
+
+      fakeFrontendComponentApi.get('/footer').matchHeader('x-user-token', userToken).reply(200, expectedResponse)
+
+      // When
+      const actual = await frontendComponentApiClient.getComponent('footer', userToken)
+
+      // Then
+      expect(actual).toEqual(expectedResponse)
+      expect(nock.isDone()).toBe(true)
+    })
+  })
+})
