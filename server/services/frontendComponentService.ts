7c96edb4a819b1d42d914c3abf455470ad270668
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-09-12 17:25:48 +0100

    RR-296 add some basic error handling for the frontend component service

--- server/services/frontendComponentService.ts
+++ server/services/frontendComponentService.ts
@@ -1,10 +1,17 @@
 import type { FrontendComponent } from 'frontendComponentApiClient'
 import { FrontendComponentApiClient } from '../data'
+import logger from '../../logger'
 
 export default class FrontendComponentService {
   constructor(private readonly frontendApiClient: FrontendComponentApiClient) {}
 
   async getComponents(component: 'header' | 'footer', token: string): Promise<FrontendComponent> {
-    return this.frontendApiClient.getComponents(component, token)
+    try {
+      const frontendComponentResponse = await this.frontendApiClient.getComponents(component, token)
+      return frontendComponentResponse
+    } catch (error) {
+      logger.error(`Error retrieving DPS frontend components: ${error}`)
+      return { problemRetrievingData: true }
+    }
   }
 }
