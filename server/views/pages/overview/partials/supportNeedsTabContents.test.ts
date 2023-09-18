20e388c0ac37495f497f77869a28830501eebed3
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2023-09-18 10:51:18 +0100

    RR-315 - Refactor Neurodiversity panel based on new design (#176)

--- server/views/pages/overview/partials/supportNeedsTabContents.test.ts
+++ server/views/pages/overview/partials/supportNeedsTabContents.test.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs'
-import cheerio from 'cheerio'
+import cheerio, { Cheerio, CheerioAPI } from 'cheerio'
 import nunjucks, { Template } from 'nunjucks'
 import { registerNunjucks } from '../../../../utils/nunjucksSetup'
 import aValidPrisonerSupportNeeds from '../../../../testsupport/supportNeedsTestDataBuilder'
@@ -25,57 +25,38 @@ describe('Support Needs tab view', () => {
 
     // When
     const $ = cheerio.load(compiledTemplate.render(viewContext))
+    setupCheerioExtensionFunctions($)
 
     // Then
     const healthAndSupportNeedsCard = $('#health-and-support-needs-summary-card')
     // expect there to be only 1 gov-uk-summmary-list representing the data from the 1 prison in aValidPrisonerSupportNeeds()
     expect(healthAndSupportNeedsCard.find('.govuk-summary-list').length).toEqual(1)
-    expect(
-      healthAndSupportNeedsCard //
-        .find('h3')
-        .text(),
-    ).toContain(`Jimmy Lightfingers's learning difficulties, disabilities and health needs recorded whilst at`)
-    expect(
-      healthAndSupportNeedsCard //
-        .find('h3')
-        .text(),
-    ).toContain('MOORLAND (HMP & YOI)')
-    expect(
-      healthAndSupportNeedsCard //
-        .find(`.govuk-summary-list__key:contains('Rapid assessment')`)
-        .next()
-        .text()
-        .trim(),
-    ).toEqual('18 February 2022')
-    expect(
-      healthAndSupportNeedsCard //
-        .find(`.govuk-summary-list__key:contains('In-depth assessment')`)
-        .next()
-        .text()
-        .trim(),
-    ).toEqual('Not recorded in Curious')
-    expect(
-      healthAndSupportNeedsCard //
-        .find(`.govuk-summary-list__key:contains('Primary LDD and health needs')`)
-        .next()
-        .text()
-        .trim(),
-    ).toEqual('Visual impairment')
-    expect(
-      healthAndSupportNeedsCard //
-        .find(`.govuk-summary-list__key:contains('Additional LDD and health needs')`)
-        .next()
-        .find('li')
-        .toArray()
-        .map(el => $(el).text()),
-    ).toEqual(['Hearing impairment', 'Mental health difficulty', 'Social and emotional difficulties'])
+    expect(healthAndSupportNeedsCard.heading()).toContain(
+      `Jimmy Lightfingers's learning difficulties, disabilities and health needs recorded whilst at`,
+    )
+    expect(healthAndSupportNeedsCard.heading()).toContain('MOORLAND (HMP & YOI)')
+    expect(healthAndSupportNeedsCard.rapidAssessmentDate()).toEqual('18 February 2022')
+    expect(healthAndSupportNeedsCard.inDepthAssessmentDate()).toEqual('Not recorded in Curious')
+    expect(healthAndSupportNeedsCard.primaryLddAndHealthNeeds()).toEqual('Visual impairment')
+    expect(healthAndSupportNeedsCard.additionalLddAndHealthNeeds()).toEqual([
+      'Hearing impairment',
+      'Mental health difficulty',
+      'Social and emotional difficulties',
+    ])
 
-    const summaryListRow = '.govuk-summary-list__row'
-    const neurodiversityCard = '#neurodiversity-summary-card'
-    const supportNeeded = `${neurodiversityCard} ${summaryListRow}:nth-of-type(1) .govuk-list:nth-of-type(1) li:nth-of-type(1)`
-    const neurodiversity = `${neurodiversityCard} ${summaryListRow}:nth-of-type(2) .govuk-list:nth-of-type(1) li:nth-of-type(1)`
-    expect($(`${supportNeeded}`).text()).toContain('Writing support')
-    expect($(`${neurodiversity}`).text()).toContain('Dyslexia')
+    const neurodiversityCard = $('#neurodiversity-summary-card')
+    // expect there to be only 1 gov-uk-summmary-list representing the data from the 1 prison in aValidPrisonerSupportNeeds()
+    expect(neurodiversityCard.find('.govuk-summary-list').length).toEqual(1)
+    expect(neurodiversityCard.heading()).toContain(
+      `Jimmy Lightfingers's neurodiversity support needs recorded whilst at`,
+    )
+    expect(neurodiversityCard.heading()).toContain('MOORLAND (HMP & YOI)')
+    expect(neurodiversityCard.neurodiversitySupportNeeds()).toContain('Writing support')
+    expect(neurodiversityCard.neurodiversitySupportNeeds()).toContain('Recorded on 18 February 2022')
+    expect(neurodiversityCard.assessedNeurodiversityDiagnosis()).toContain('No Identified Neurodiversity Need')
+    expect(neurodiversityCard.assessedNeurodiversityDiagnosis()).toContain('Recorded on 18 May 2022')
+    expect(neurodiversityCard.selfDeclaredNeurodiversityDiagnosis()).toContain('Dyslexia')
+    expect(neurodiversityCard.selfDeclaredNeurodiversityDiagnosis()).toContain('Recorded on 18 February 2022')
   })
 
   it('should render content saying curious is unavailable given problem retrieving data is true', () => {
@@ -95,3 +76,55 @@ describe('Support Needs tab view', () => {
     expect($('h2').text()).toEqual('Sorry, the Curious service is currently unavailable.')
   })
 })
+
+declare module 'cheerio' {
+  interface Cheerio<T> {
+    heading(this: Cheerio<T>): Cheerio<T>
+    rapidAssessmentDate(this: Cheerio<T>): Cheerio<T>
+    inDepthAssessmentDate(this: Cheerio<T>): Cheerio<T>
+    primaryLddAndHealthNeeds(this: Cheerio<T>): Cheerio<T>
+    additionalLddAndHealthNeeds(this: Cheerio<T>): Cheerio<T>
+    selfDeclaredNeurodiversityDiagnosis(this: Cheerio<T>): Cheerio<T>
+    assessedNeurodiversityDiagnosis(this: Cheerio<T>): Cheerio<T>
+    neurodiversitySupportNeeds(this: Cheerio<T>): Cheerio<T>
+  }
+}
+
+const setupCheerioExtensionFunctions = ($: CheerioAPI) => {
+  // eslint-disable-next-line no-param-reassign
+  $.prototype.heading = function heading(): Cheerio<never> {
+    return this.find('h3').text()
+  }
+  // eslint-disable-next-line no-param-reassign
+  $.prototype.rapidAssessmentDate = function rapidAssessmentDate(): Cheerio<never> {
+    return this.find(`.govuk-summary-list__key:contains('Rapid assessment')`).next().text().trim()
+  }
+  // eslint-disable-next-line no-param-reassign
+  $.prototype.inDepthAssessmentDate = function inDepthAssessmentDate(): Cheerio<never> {
+    return this.find(`.govuk-summary-list__key:contains('In-depth assessment')`).next().text().trim()
+  }
+  // eslint-disable-next-line no-param-reassign
+  $.prototype.primaryLddAndHealthNeeds = function primaryLddAndHealthNeeds(): Cheerio<never> {
+    return this.find(`.govuk-summary-list__key:contains('Primary LDD and health needs')`).next().text().trim()
+  }
+  // eslint-disable-next-line no-param-reassign
+  $.prototype.additionalLddAndHealthNeeds = function additionalLddAndHealthNeeds(): Cheerio<never> {
+    return this.find(`.govuk-summary-list__key:contains('Additional LDD and health needs')`)
+      .next()
+      .find('li')
+      .toArray()
+      .map((el: never) => $(el).text())
+  }
+  // eslint-disable-next-line no-param-reassign
+  $.prototype.selfDeclaredNeurodiversityDiagnosis = function selfDeclaredNeurodiversityDiagnosis(): Cheerio<never> {
+    return this.find(`.govuk-summary-list__key:contains('Self-declared')`).next().text()
+  }
+  // eslint-disable-next-line no-param-reassign
+  $.prototype.assessedNeurodiversityDiagnosis = function assessedNeurodiversityDiagnosis(): Cheerio<never> {
+    return this.find(`.govuk-summary-list__key:contains('From neurodiversity assessment')`).next().text()
+  }
+  // eslint-disable-next-line no-param-reassign
+  $.prototype.neurodiversitySupportNeeds = function neurodiversitySupportNeeds(): Cheerio<never> {
+    return this.find(`.govuk-summary-list__key:contains('Support needed')`).next().text()
+  }
+}
