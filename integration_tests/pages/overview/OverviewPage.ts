62c9656cfb0fbb5e91025b125738f098a29e41db
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-10-05 14:39:54 +0100

    RR-353 update the curious unavailable test to be less specific to content

--- integration_tests/pages/overview/OverviewPage.ts
+++ integration_tests/pages/overview/OverviewPage.ts
@@ -134,7 +134,7 @@ export default class OverviewPage extends Page {
   }
 
   hasCuriousUnavailableMessageDisplayed() {
-    cy.get('h2').contains('We cannot show these details from Curious right now')
+    this.curiousUnavailableMessage().should('be.exist')
     return this
   }
 
@@ -189,4 +189,6 @@ export default class OverviewPage extends Page {
   createCiagInductionLink = (): PageElement => cy.get('[data-qa=link-to-create-ciag-induction]')
 
   notesExpander = (idx: number): PageElement => cy.get(`[data-qa=overview-notes-expander-${idx}]`)
+
+  curiousUnavailableMessage = (): PageElement => cy.get('[data-qa=curious-unavailable-message]')
 }
