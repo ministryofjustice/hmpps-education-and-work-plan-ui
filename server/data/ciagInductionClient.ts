5a72f95eddc19f8aaf2d873229bab8b134b124ea
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2023-10-17 13:08:30 +0100

    RR-395 - Imported latest CIAG API swagger spec; added client method to get CIAG Inductions for prisoner list (#253)

--- server/data/ciagInductionClient.ts
+++ server/data/ciagInductionClient.ts
@@ -1,4 +1,4 @@
-import type { CiagInduction } from 'ciagInductionApiClient'
+import type { CiagInduction, CiagInductionListResponse, CiagInductionListRequest } from 'ciagInductionApiClient'
 import RestClient from './restClient'
 import config from '../config'
 
@@ -8,9 +8,16 @@ export default class CiagInductionClient {
   }
 
   async getCiagInduction(prisonNumber: string, token: string): Promise<CiagInduction> {
-    const ciagInduction = CiagInductionClient.restClient(token).get({
+    return CiagInductionClient.restClient(token).get({
       path: `/ciag/induction/${prisonNumber}`,
     })
-    return ciagInduction as Promise<CiagInduction>
+  }
+
+  async getCiagInductionsForPrisonNumbers(prisonNumbers: string[], token: string): Promise<CiagInductionListResponse> {
+    const requestBody: CiagInductionListRequest = { offenderIds: prisonNumbers }
+    return CiagInductionClient.restClient(token).post({
+      path: '/ciag/induction/list',
+      data: requestBody,
+    })
   }
 }
