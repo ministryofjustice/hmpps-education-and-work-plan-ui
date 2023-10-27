f253624857686a30a1abb0e716969c5a44fc054a
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-10-27 10:24:08 +0100

    RR-441 create the UI for the education and training short route

---
+++ server/filters/formatInPrisonEducationFilter.test.ts
@@ -0,0 +1,25 @@
+import formatInPrisonEducationFilter from './formatInPrisonEducationFilter'
+
+describe('formatInPrisonEducationFilter', () => {
+  describe('should format view model values', () => {
+    Array.of(
+      { source: 'BARBERING_AND_HAIRDRESSING', expected: 'Barbering and hairdressing' },
+      { source: 'CATERING', expected: 'Catering' },
+      { source: 'COMMUNICATION_SKILLS', expected: 'Communication skills' },
+      { source: 'ENGLISH_LANGUAGE_SKILLS', expected: 'English language skills' },
+      { source: 'FORKLIFT_DRIVING', expected: 'Forklift driving' },
+      { source: 'INTERVIEW_SKILLS', expected: 'Interview skills' },
+      { source: 'MACHINERY_TICKETS', expected: 'Machinery tickets' },
+      { source: 'NUMERACY_SKILLS', expected: 'Numeracy skills' },
+      { source: 'RUNNING_A_BUSINESS', expected: 'Running a business' },
+      { source: 'SOCIAL_AND_LIFE_SKILLS', expected: 'Social and life skills' },
+      { source: 'WELDING_AND_METALWORK', expected: 'Welding and metalwork' },
+      { source: 'WOODWORK_AND_JOINERY', expected: 'Woodwork and joinery' },
+      { source: 'OTHER', expected: 'Other' },
+    ).forEach(spec => {
+      it(`source: ${spec.source}, expected: ${spec.expected}`, () => {
+        expect(formatInPrisonEducationFilter(spec.source)).toEqual(spec.expected)
+      })
+    })
+  })
+})
