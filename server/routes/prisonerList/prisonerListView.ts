85941b0b2aac191769f9fd5feba95deae525e630
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-11-06 11:52:48 +0000

    RR-438 initial UI implementation for pagination

--- server/routes/prisonerList/prisonerListView.ts
+++ server/routes/prisonerList/prisonerListView.ts
@@ -12,11 +12,74 @@ export default class PrisonerListView {
     currentPageOfRecords: PrisonerSearchSummary[]
     searchTerm?: string
     statusFilter?: string
+    renderPaginationControls: boolean
+    items: Item[]
+    results: Results
+    previousPage: Paging
+    nextPage: Paging
   } {
     return {
       currentPageOfRecords: this.pagedPrisonerSearchSummary.getCurrentPage(),
       searchTerm: this.searchTerm,
       statusFilter: this.statusFilter,
+      renderPaginationControls: this.pagedPrisonerSearchSummary.totalPages > 1,
+      items: buildItemsArray(this.pagedPrisonerSearchSummary),
+      results: buildResults(this.pagedPrisonerSearchSummary),
+      previousPage: getPreviousPage(this.pagedPrisonerSearchSummary),
+      nextPage: getNextPage(this.pagedPrisonerSearchSummary),
     }
   }
+}
+
+const buildItemsArray = (pagedPrisonerSearchSummary: PagedPrisonerSearchSummary): Item[] => {
+  const items: Item[] = []
+  for (let i = 1; i <= pagedPrisonerSearchSummary.totalPages; i += 1) {
+    items.push({
+      type: undefined,
+      text: i.toString(),
+      href: `?page=${i}`,
+      selected: i === pagedPrisonerSearchSummary.currentPageNumber,
+    })
+  }
+  return items
+}
+
+const buildResults = (pagedPrisonerSearchSummary: PagedPrisonerSearchSummary): Results => ({
+  count: pagedPrisonerSearchSummary.totalResults,
+  from: pagedPrisonerSearchSummary.resultIndexFrom,
+  to: pagedPrisonerSearchSummary.resultIndexTo,
+})
+
+const getPreviousPage = (pagedPrisonerSearchSummary: PagedPrisonerSearchSummary): Paging => {
+  const previousPageNumber = pagedPrisonerSearchSummary.currentPageNumber - 1
+  return {
+    text: 'Previous',
+    href: previousPageNumber > 1 ? `?page=${previousPageNumber}` : '',
+  }
+}
+
+const getNextPage = (pagedPrisonerSearchSummary: PagedPrisonerSearchSummary): Paging => {
+  const nextPageNumber = pagedPrisonerSearchSummary.currentPageNumber + 1
+  return {
+    text: 'Next',
+    href: nextPageNumber <= pagedPrisonerSearchSummary.totalPages ? `?page=${nextPageNumber}` : '',
+  }
+}
+
+interface Item {
+  type?: string
+  text: string
+  href: string
+  selected: boolean
+}
+
+interface Results {
+  count: number
+  from: number
+  to: number
+}
+
+interface Paging {
+  text: string
+  href: string
 }
