916eac1889748ba241dd86b46297fe5deaa9e6ac
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2023-11-15 15:06:24 +0000

    RR-470 - configured ClickAnalyticsPlugin to send click events to App Insights (#332)

--- assets/js/applicationinsights.js
+++ assets/js/applicationinsights.js
@@ -1,13 +1,26 @@
+/* global Microsoft */
 window.applicationInsights = (function () {
   let appInsights
 
   return {
     init: (connectionString, applicationInsightsRoleName, authenticatedUser) => {
       if (!appInsights && connectionString) {
+        const clickPluginInstance = new Microsoft.ApplicationInsights.ClickAnalyticsPlugin()
+        const clickPluginConfig = {
+          autoCapture: true,
+          dataTags: {
+            useDefaultContentNameOrId: true,
+          },
+        }
+
         appInsights = new Microsoft.ApplicationInsights.ApplicationInsights({
           config: {
             connectionString,
             autoTrackPageVisitTime: true,
+            extensions: [clickPluginInstance],
+            extensionConfig: {
+              [clickPluginInstance.identifier]: clickPluginConfig,
+            },
           },
         })
         appInsights.addTelemetryInitializer(envelope => {
