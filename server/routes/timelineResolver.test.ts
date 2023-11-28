6ed7abea29f24d6435ff0a6c6545097fb46c4138
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2023-11-28 21:12:51 +0000

    RR-475 use the correct view model type in timeline resolver tests

--- server/routes/timelineResolver.test.ts
+++ server/routes/timelineResolver.test.ts
@@ -1,47 +1,92 @@
-import type { TimelineResponse } from 'educationAndWorkPlanApiClient'
+import moment from 'moment'
+import type { Timeline } from 'viewModels'
 import filterTimelineEvents from './timelineResolver'
 
 describe('timelineResolver', () => {
   describe('filterTimelineEvents', () => {
-    it('should return Timeline events with MULTIPLE_GOALS_CREATED with the same correlationId value grouped and events sorted by timestamp', () => {
+    it('should return a Timeline with multiple goal events with the same correlationId value grouped and events sorted by timestamp', () => {
       // Given
-      const timelineResponse = {
+      const timeline = {
+        prisonNumber: 'A1234AA',
+        problemRetrievingData: false,
         events: [
           {
+            reference: 'cd98ea4c-b415-48d9-a600-9068cefe65e4x',
+            sourceReference: '33bc1045-7368-47c4-a261-4d616b7b51b9',
             eventType: 'GOAL_CREATED',
-            timestamp: '2023-01-01T00:00:00.000Z',
+            prison: {
+              prisonId: 'MDI',
+              prisonName: undefined,
+            },
+            timestamp: moment('2023-09-01T10:47:38.565Z').toDate(),
             correlationId: '246aa049-c5df-459d-8231-bdeab3936d0f',
+            contextualInfo: '',
+            actionedByDisplayName: 'Ralph Gen',
           },
           {
-            timestamp: '2023-01-01T00:00:00.000Z',
+            reference: 'df98ea4c-b415-48d9-a600-9068cbaa66f6t',
+            sourceReference: '64bc1045-7368-47c4-a261-4d616b7b51b9',
             eventType: 'GOAL_CREATED',
+            prison: {
+              prisonId: 'MDI',
+              prisonName: undefined,
+            },
+            timestamp: moment('2023-09-01T10:47:38.565Z').toDate(),
             correlationId: '246aa049-c5df-459d-8231-bdeab3936d0f',
+            contextualInfo: '',
+            actionedByDisplayName: 'Ralph Gen',
           },
           {
-            timestamp: '2023-01-02T00:00:00.000Z',
+            reference: 'cd98ea4c-b415-48d9-a600-2423cefe65et',
+            sourceReference: '44bc1011-4368-47c4-a261-4d515b7b51c9',
             eventType: 'GOAL_CREATED',
-            correlationId: '847aa5ad-2068-40e1-aec0-66b19007c494',
+            prison: {
+              prisonId: 'MDI',
+              prisonName: undefined,
+            },
+            timestamp: moment('2023-10-01T10:47:38.565Z').toDate(),
+            correlationId: '524aa049-c5df-459d-8231-bdeab3425c1d',
+            contextualInfo: '',
+            actionedByDisplayName: 'Ralph Gen',
           },
         ],
-      } as TimelineResponse
+      } as Timeline
 
       const expected = {
+        prisonNumber: 'A1234AA',
+        problemRetrievingData: false,
         events: [
           {
-            timestamp: '2023-01-02T00:00:00.000Z',
+            reference: 'cd98ea4c-b415-48d9-a600-2423cefe65et',
+            sourceReference: '44bc1011-4368-47c4-a261-4d515b7b51c9',
             eventType: 'GOAL_CREATED',
-            correlationId: '847aa5ad-2068-40e1-aec0-66b19007c494',
+            prison: {
+              prisonId: 'MDI',
+              prisonName: undefined,
+            },
+            timestamp: moment('2023-10-01T10:47:38.565Z').toDate(),
+            correlationId: '524aa049-c5df-459d-8231-bdeab3425c1d',
+            contextualInfo: '',
+            actionedByDisplayName: 'Ralph Gen',
           },
           {
+            reference: 'df98ea4c-b415-48d9-a600-9068cbaa66f6t',
+            sourceReference: '64bc1045-7368-47c4-a261-4d616b7b51b9',
             eventType: 'MULTIPLE_GOALS_CREATED',
-            timestamp: '2023-01-01T00:00:00.000Z',
+            prison: {
+              prisonId: 'MDI',
+              prisonName: undefined,
+            },
+            timestamp: moment('2023-09-01T10:47:38.565Z').toDate(),
             correlationId: '246aa049-c5df-459d-8231-bdeab3936d0f',
+            contextualInfo: '',
+            actionedByDisplayName: 'Ralph Gen',
           },
         ],
-      } as TimelineResponse
+      } as Timeline
 
       // When
-      const actual = filterTimelineEvents(timelineResponse)
+      const actual = filterTimelineEvents(timeline)
 
       // Then
       expect(actual).toEqual(expected)
