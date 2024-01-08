f858a9dd9f149c89aa52a23f9b1fcc7d9ebcf71f
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-01-08 14:30:07 +0000

    Revert "RR-501 - Simplified logic re: session handling when editing steps"
    This reverts commit 6edc9dd47d73c1043be6337d122c2ec6fdbe7ea7.

--- server/routes/createGoal/createGoalController.ts
+++ server/routes/createGoal/createGoalController.ts
@@ -84,7 +84,8 @@ export default class CreateGoalController {
 
     if (isEditMode(req)) {
       req.session.newGoal.addStepForm =
-        req.session.newGoals[parseInt(goalIndex, 10) - 1].addStepForms[parseInt(stepIndex, 10) - 1]
+        req.session.newGoals[parseInt(goalIndex, 10) - 1].addStepForms[parseInt(stepIndex, 10) - 1] ||
+        req.session.newGoal.addStepForm
     } else if (!req.session.newGoal.addStepForm) {
       req.session.newGoal.addStepForm = { stepNumber: 1 }
     }
@@ -135,6 +136,8 @@ export default class CreateGoalController {
       if (!req.session.newGoals[parseInt(goalIndex, 10) - 1].addStepForms[parseInt(stepIndex, 10) - 1]) {
         return next(createError(404, `Step ${stepIndex} not found`))
       }
+      req.session.newGoal.addStepForm =
+        req.session.newGoals[parseInt(goalIndex, 10) - 1].addStepForms[parseInt(stepIndex, 10) - 1]
       req.session.newGoal = undefined
       return res.redirect(`/plan/${prisonNumber}/goals/review`)
     }
