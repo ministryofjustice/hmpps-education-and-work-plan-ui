a51d3a94cd94214cb53091c9cfce39eb381bc16e
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2024-01-16 14:18:42 +0000

    RR-392 add check for data to prevent app crashing if non present

--- server/routes/overview/overviewController.ts
+++ server/routes/overview/overviewController.ts
@@ -103,28 +103,32 @@ export default class OverviewController {
     const supportNeeds = await this.curiousService.getPrisonerSupportNeeds(prisonNumber, req.user.username)
 
     // Loop through the healthAndSupport needs array and update the prison name for each need
-    await Promise.all(
-      supportNeeds.healthAndSupportNeeds.map(async supportNeed => {
-        const prison = await this.prisonService.lookupPrison(supportNeed.prisonId, req.user.username)
-        if (prison) {
-          // TODO refactor to avoid param-reassign eslint rule
-          // eslint-disable-next-line no-param-reassign
-          supportNeed.prisonName = prison.prisonName
-        }
-      }),
-    )
+    if (supportNeeds.healthAndSupportNeeds) {
+      await Promise.all(
+        supportNeeds.healthAndSupportNeeds.map(async supportNeed => {
+          const prison = await this.prisonService.lookupPrison(supportNeed.prisonId, req.user.username)
+          if (prison) {
+            // TODO refactor to avoid param-reassign eslint rule
+            // eslint-disable-next-line no-param-reassign
+            supportNeed.prisonName = prison.prisonName
+          }
+        }),
+      )
+    }
 
     // Loop through the neurodiversities needs array and update the prison name for each need
-    await Promise.all(
-      supportNeeds.neurodiversities.map(async supportNeed => {
-        const prison = await this.prisonService.lookupPrison(supportNeed.prisonId, req.user.username)
-        if (prison) {
-          // TODO refactor to avoid param-reassign eslint rule
-          // eslint-disable-next-line no-param-reassign
-          supportNeed.prisonName = prison.prisonName
-        }
-      }),
-    )
+    if (supportNeeds.neurodiversities) {
+      await Promise.all(
+        supportNeeds.neurodiversities.map(async supportNeed => {
+          const prison = await this.prisonService.lookupPrison(supportNeed.prisonId, req.user.username)
+          if (prison) {
+            // TODO refactor to avoid param-reassign eslint rule
+            // eslint-disable-next-line no-param-reassign
+            supportNeed.prisonName = prison.prisonName
+          }
+        }),
+      )
+    }
 
     const view = new SupportNeedsView(prisonerSummary, supportNeeds)
     res.render('pages/overview/index', { ...view.renderArgs })
