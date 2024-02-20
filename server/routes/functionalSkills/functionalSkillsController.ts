309a5731807b8757f755d149d422ef661bbad62a
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2024-02-20 14:24:11 +0000

    RR-567 improve the repeated promise all for prison names

--- server/routes/functionalSkills/functionalSkillsController.ts
+++ server/routes/functionalSkills/functionalSkillsController.ts
@@ -24,21 +24,26 @@ export default class FunctionalSkillsController {
     const mathsSkills = functionalSkillsByType(functionalSkillsFromCurious.assessments, 'MATHS')
     const digitalSkills = functionalSkillsByType(functionalSkillsFromCurious.assessments, 'DIGITAL_LITERACY')
 
+    // Handle the prison name lookup for each assessment
+    // Define the types of assessments that we're interested in
     const assessmentTypes: ('ENGLISH' | 'MATHS' | 'DIGITAL_LITERACY')[] = ['ENGLISH', 'MATHS', 'DIGITAL_LITERACY']
 
+    // Get all assessments of the specified types
     const allSkillsByType = assessmentTypes.flatMap(type =>
       functionalSkillsByType(functionalSkillsFromCurious.assessments, type),
     )
 
-    await Promise.all(
-      allSkillsByType.map(async assessment => {
-        const prison = await this.prisonService.lookupPrison(assessment.prisonId, req.user.username)
-        if (prison) {
-          // eslint-disable-next-line no-param-reassign
-          assessment.prisonName = prison.prisonName
-        }
-      }),
-    )
+    // Function to add the prison name to an assessment
+    const addPrisonNameToAssessment = async (assessment: { prisonId: string; prisonName: string }) => {
+      const prison = await this.prisonService.lookupPrison(assessment.prisonId, req.user.username)
+      if (prison) {
+        // eslint-disable-next-line no-param-reassign
+        assessment.prisonName = prison.prisonName
+      }
+    }
+
+    // Add the prison name to all assessments
+    await Promise.all(allSkillsByType.map(addPrisonNameToAssessment))
 
     const view = new FunctionalSkillsView(
       prisonerSummary,
