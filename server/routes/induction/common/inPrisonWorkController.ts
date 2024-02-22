6b2088e0a030d71bd21266ca0e3643e407034781
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2024-02-22 13:37:16 +0000

    RR-647 initial work for displaying the form values from the enum

--- server/routes/induction/common/inPrisonWorkController.ts
+++ server/routes/induction/common/inPrisonWorkController.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, RequestHandler, Response } from 'express'
 import InPrisonWorkView from './inPrisonWorkView'
 import InductionController from './inductionController'
 import InPrisonWorkValue from '../../../enums/inPrisonWorkValue'
+import formatInPrisonWorkInterestFilter from '../../../filters/formatInPrisonWorkInterestFilter'
 
 /**
  * Abstract controller class defining functionality common to both the Create and Update Induction journeys.
@@ -22,11 +23,22 @@ export default abstract class InPrisonWorkController extends InductionController
     const inPrisonWorkForm = req.session.inPrisonWorkForm || toInPrisonWorkForm(inductionDto)
     req.session.inPrisonWorkForm = undefined
 
+    const formValues = Object.values(InPrisonWorkValue)
+      .filter(value => value !== 'OTHER')
+      .map(value => {
+        return {
+          value,
+          text: formatInPrisonWorkInterestFilter(value),
+          checked: inPrisonWorkForm.inPrisonWork.includes(value),
+        }
+      })
+
     const view = new InPrisonWorkView(
       prisonerSummary,
       this.getBackLinkUrl(req),
       this.getBackLinkAriaText(req),
       inPrisonWorkForm,
+      formValues,
       req.flash('errors'),
     )
     return res.render('pages/induction/inPrisonWork/index', { ...view.renderArgs })
--- server/routes/induction/common/inPrisonWorkView.ts
+++ server/routes/induction/common/inPrisonWorkView.ts
@@ -7,6 +7,7 @@ export default class InPrisonWorkView {
     private readonly backLinkUrl: string,
     private readonly backLinkAriaText: string,
     private readonly inPrisonWorkForm: InPrisonWorkForm,
+    private readonly formValues: Array<{ value: string; text: string }>,
     private readonly errors?: Array<Record<string, string>>,
   ) {}
 
@@ -15,6 +16,7 @@ export default class InPrisonWorkView {
     backLinkUrl: string
     backLinkAriaText: string
     form: InPrisonWorkForm
+    formValues: Array<{ value: string; text: string }>
     errors?: Array<Record<string, string>>
   } {
     return {
@@ -22,6 +24,7 @@ export default class InPrisonWorkView {
       backLinkUrl: this.backLinkUrl,
       backLinkAriaText: this.backLinkAriaText,
       form: this.inPrisonWorkForm,
+      formValues: this.formValues,
       errors: this.errors || [],
     }
   }
