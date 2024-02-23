9279ab9206dc2313b2eb8aaaf2ab7f7ab0a87c5a
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-02-23 13:57:02 +0000

    RR-645 - Define form objects and wire into the views

--- server/routes/induction/common/skillsController.ts
+++ server/routes/induction/common/skillsController.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
+import type { InductionDto } from 'inductionDto'
+import type { SkillsForm } from 'inductionForms'
 import InductionController from './inductionController'
 import SkillsView from './skillsView'
+import SkillsValue from '../../../enums/skillsValue'
 
 /**
  * Abstract controller class defining functionality common to both the Create and Update Induction journeys.
@@ -14,14 +17,26 @@ export default abstract class SkillsController extends InductionController {
    * Returns the Skills view; suitable for use by the Create and Update journeys.
    */
   getSkillsView: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const { prisonerSummary } = req.session
+    const { prisonerSummary, inductionDto } = req.session
+
+    const skillsForm = req.session.skillsForm || toSkillsForm(inductionDto)
+    req.session.skillsForm = undefined
 
     const view = new SkillsView(
       prisonerSummary,
       this.getBackLinkUrl(req),
       this.getBackLinkAriaText(req),
+      skillsForm,
       req.flash('errors'),
     )
     return res.render('pages/induction/skills/index', { ...view.renderArgs })
+  }
+}
+
+const toSkillsForm = (inductionDto: InductionDto): SkillsForm => {
+  return {
+    skills: inductionDto.personalSkillsAndInterests.skills.map(skill => skill.skillType),
+    skillsOther: inductionDto.personalSkillsAndInterests.skills.find(skill => skill.skillType === SkillsValue.OTHER)
+      ?.skillTypeOther,
   }
 }
--- server/routes/induction/common/skillsView.ts
+++ server/routes/induction/common/skillsView.ts
@@ -1,10 +1,12 @@
 import type { PrisonerSummary } from 'viewModels'
+import type { SkillsForm } from 'inductionForms'
 
 export default class SkillsView {
   constructor(
     private readonly prisonerSummary: PrisonerSummary,
     private readonly backLinkUrl: string,
     private readonly backLinkAriaText: string,
+    private readonly skillsForm: SkillsForm,
     private readonly errors?: Array<Record<string, string>>,
   ) {}
 
@@ -12,12 +14,14 @@ export default class SkillsView {
     prisonerSummary: PrisonerSummary
     backLinkUrl: string
     backLinkAriaText: string
+    form: SkillsForm
     errors?: Array<Record<string, string>>
   } {
     return {
       prisonerSummary: this.prisonerSummary,
       backLinkUrl: this.backLinkUrl,
       backLinkAriaText: this.backLinkAriaText,
+      form: this.skillsForm,
       errors: this.errors || [],
     }
   }
