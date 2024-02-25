d974abcb2acea658589849c46757bce69b428a31
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-02-25 08:53:17 +0000

    RR-645 - Cypress tests for updating skills

---
+++ integration_tests/e2e/induction/updateSkills.cy.ts
@@ -0,0 +1,103 @@
+import Page from '../../pages/page'
+import SkillsPage from '../../pages/induction/SkillsPage'
+import WorkAndInterestsPage from '../../pages/overview/WorkAndInterestsPage'
+import SkillsValue from '../../../server/enums/skillsValue'
+import { putRequestedFor } from '../../mockApis/wiremock/requestPatternBuilder'
+import { urlEqualTo } from '../../mockApis/wiremock/matchers/url'
+import { matchingJsonPath } from '../../mockApis/wiremock/matchers/content'
+
+context('Update Skills in the Induction', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignInAsUserWithEditAuthority')
+    cy.task('stubAuthUser')
+    cy.task('stubGetHeaderComponent')
+    cy.task('stubGetFooterComponent')
+    cy.task('stubPrisonerList')
+    cy.task('stubCiagInductionList')
+    cy.task('stubActionPlansList')
+    cy.task('getPrisonerById')
+    cy.task('getActionPlan')
+    cy.task('stubLearnerProfile')
+    cy.task('stubLearnerEducation')
+    cy.task('stubUpdateInduction')
+    cy.task('stubGetInductionLongQuestionSet')
+    cy.signIn()
+  })
+
+  it.skip('should update Skills given page submitted with changed skills and no validation errors', () => {
+    // Given
+    const prisonNumber = 'G6115VJ'
+    cy.visit(`/prisoners/${prisonNumber}/induction/skills`)
+    const skillsPage = Page.verifyOnPage(SkillsPage)
+
+    // stubGetInductionLongQuestionSet has skills of Communication, Positive Attitude, Thinking & Problem Solving, and Other (Logical Thinking)
+
+    // When
+    skillsPage //
+      .deSelectSkill(SkillsValue.COMMUNICATION)
+      .deSelectSkill(SkillsValue.THINKING_AND_PROBLEM_SOLVING)
+      .chooseSkill(SkillsValue.SELF_MANAGEMENT)
+      .chooseSkill(SkillsValue.OTHER)
+      .setOtherSkillType('Circus skills')
+      .submitPage()
+
+    // Then
+    Page.verifyOnPage(WorkAndInterestsPage)
+    cy.wiremockVerify(
+      putRequestedFor(urlEqualTo(`/inductions/${prisonNumber}`)) //
+        .withRequestBody(
+          matchingJsonPath(
+            '$[?(@.personalSkillsAndInterests.skills.size() == 3 && ' +
+              "@.personalSkillsAndInterests.skills[0].skillType == 'POSITIVE_ATTITUDE' && !@.personalSkillsAndInterests.skills[0].skillTypeOther && " +
+              "@.personalSkillsAndInterests.skills[1].skillType == 'SELF_MANAGEMENT' && !@.personalSkillsAndInterests.skills[1].skillTypeOther && " +
+              "@.personalSkillsAndInterests.skills[2].skillType == 'OTHER' && @.personalSkillsAndInterests.skills[2].skillTypeOther == 'Circus skills')]",
+          ),
+        ),
+    )
+  })
+
+  it('should update Skills given page submitted with changed skills values to NONE and no validation errors', () => {
+    // Given
+    const prisonNumber = 'G6115VJ'
+    cy.visit(`/prisoners/${prisonNumber}/induction/skills`)
+    const skillsPage = Page.verifyOnPage(SkillsPage)
+
+    // stubGetInductionLongQuestionSet has skills of Communication, Positive Attitude, Thinking & Problem Solving, and Other (Logical Thinking)
+
+    // When
+    skillsPage //
+      .chooseSkill(SkillsValue.NONE)
+      .submitPage()
+
+    // Then
+    Page.verifyOnPage(WorkAndInterestsPage)
+    cy.wiremockVerify(
+      putRequestedFor(urlEqualTo(`/inductions/${prisonNumber}`)) //
+        .withRequestBody(
+          matchingJsonPath(
+            '$[?(@.personalSkillsAndInterests.skills.size() == 1 && ' +
+              "@.personalSkillsAndInterests.skills[0].skillType == 'NONE' && !@.personalSkillsAndInterests.skills[0].skillTypeOther)]",
+          ),
+        ),
+    )
+  })
+
+  it('should not update Skills given page submitted with validation errors', () => {
+    // Given
+    const prisonNumber = 'G6115VJ'
+    cy.visit(`/prisoners/${prisonNumber}/induction/skills`)
+    let skillsPage = Page.verifyOnPage(SkillsPage)
+
+    // When
+    skillsPage //
+      .chooseSkill(SkillsValue.OTHER)
+      .clearOtherSkillType()
+      .submitPage()
+
+    // Then
+    skillsPage = Page.verifyOnPage(SkillsPage)
+    skillsPage.hasFieldInError('skillsOther')
+    cy.wiremockVerifyNoInteractions(putRequestedFor(urlEqualTo(`/inductions/${prisonNumber}`)))
+  })
+})
