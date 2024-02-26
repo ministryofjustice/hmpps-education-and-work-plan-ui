4c4d789cd3ba2af6202c1386b64b226a17380564
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-02-26 09:20:40 +0000

    NO_JIRA - fix the build by removing hard coded dates

--- server/routes/createGoal/createGoalFormValidator.test.ts
+++ server/routes/createGoal/createGoalFormValidator.test.ts
@@ -1,10 +1,16 @@
 import type { CreateGoalForm } from 'forms'
 import validateGoalTitle from '../../validators/goalTitleValidator'
+import goalTargetCompletionDateValidator from '../../validators/goalTargetCompletionDateValidator'
 import validateCreateGoalForm from './createGoalFormValidator'
 
 jest.mock('../../validators/goalTitleValidator')
+jest.mock('../../validators/goalTargetCompletionDateValidator')
+
 describe('createGoalFormValidator', () => {
   const mockedValidateGoalTitle = validateGoalTitle as jest.MockedFunction<typeof validateGoalTitle>
+  const mockedValidateTargetCompletionDate = goalTargetCompletionDateValidator as jest.MockedFunction<
+    typeof goalTargetCompletionDateValidator
+  >
 
   it('should validate given validators return no errors', () => {
     // Given
@@ -15,6 +21,7 @@ describe('createGoalFormValidator', () => {
     } as CreateGoalForm
 
     mockedValidateGoalTitle.mockReturnValue([])
+    mockedValidateTargetCompletionDate.mockReturnValue([])
 
     // When
     const errors = validateCreateGoalForm(form)
@@ -31,11 +38,29 @@ describe('createGoalFormValidator', () => {
     } as CreateGoalForm
 
     mockedValidateGoalTitle.mockReturnValue(['some-title-error'])
+    mockedValidateTargetCompletionDate.mockReturnValue([])
 
     // When
     const errors = validateCreateGoalForm(form)
 
     // Then
     expect(errors).toEqual([{ href: '#title', text: 'some-title-error' }])
+  })
+
+  it('should validate given goal title errors', () => {
+    const form = {
+      prisonNumber: 'A1234BC',
+      title: 'Learn Spanish',
+      targetCompletionDate: '',
+    } as CreateGoalForm
+
+    mockedValidateGoalTitle.mockReturnValue([])
+    mockedValidateTargetCompletionDate.mockReturnValue(['some-target-completion-date-error'])
+
+    // When
+    const errors = validateCreateGoalForm(form)
+
+    // Then
+    expect(errors).toEqual([{ href: '#targetCompletionDate', text: 'some-target-completion-date-error' }])
   })
 })
