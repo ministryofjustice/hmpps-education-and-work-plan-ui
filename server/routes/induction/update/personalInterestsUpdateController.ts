bd7c39f20ce9d52a278c52d8e700b7a3afdff858
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2024-02-26 15:00:48 +0000

    RR-645 handle personal interests form submission (#441)

--- server/routes/induction/update/personalInterestsUpdateController.ts
+++ server/routes/induction/update/personalInterestsUpdateController.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
+import createError from 'http-errors'
+import type { InductionDto, PersonalInterestDto } from 'inductionDto'
+import type { PersonalInterestsForm } from 'inductionForms'
 import PersonalInterestsController from '../common/personalInterestsController'
+import toCreateOrUpdateInductionDto from '../../../data/mappers/createOrUpdateInductionDtoMapper'
+import logger from '../../../../logger'
+import { InductionService } from '../../../services'
+import validatePersonalInterestsForm from './personalInterestsFormValidator'
+import PersonalInterestsValue from '../../../enums/personalInterestsValue'
 
 /**
  * Controller for the Update of the Personal Interests screen of the Induction.
  */
 export default class PersonalInterestsUpdateController extends PersonalInterestsController {
-  constructor() {
+  constructor(private readonly inductionService: InductionService) {
     super()
   }
 
@@ -19,8 +27,61 @@ export default class PersonalInterestsUpdateController extends PersonalInterests
   }
 
   submitPersonalInterestsForm: RequestHandler = async (
-    _req: Request,
-    _res: Response,
-    _next: NextFunction,
-  ): Promise<void> => {}
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    const { prisonNumber } = req.params
+    const { prisonerSummary, inductionDto } = req.session
+    const { prisonId } = prisonerSummary
+
+    req.session.personalInterestsForm = { ...req.body }
+    if (!req.session.personalInterestsForm.personalInterests) {
+      req.session.personalInterestsForm.personalInterests = []
+    }
+    if (!Array.isArray(req.session.personalInterestsForm.personalInterests)) {
+      req.session.personalInterestsForm.personalInterests = [req.session.personalInterestsForm.personalInterests]
+    }
+    const { personalInterestsForm } = req.session
+
+    const errors = validatePersonalInterestsForm(personalInterestsForm, prisonerSummary)
+    if (errors.length > 0) {
+      req.flash('errors', errors)
+      return res.redirect(`/prisoners/${prisonNumber}/induction/personal-interests`)
+    }
+
+    const updatedInduction = this.updatedInductionDtoWithPersonalInterests(inductionDto, personalInterestsForm)
+    const updateInductionDto = toCreateOrUpdateInductionDto(prisonId, updatedInduction)
+
+    try {
+      await this.inductionService.updateInduction(prisonNumber, updateInductionDto, req.user.token)
+
+      req.session.personalInterestsForm = undefined
+      req.session.inductionDto = undefined
+      return res.redirect(`/plan/${prisonNumber}/view/work-and-interests`)
+    } catch (e) {
+      logger.error(`Error updating Induction for prisoner ${prisonNumber}`, e)
+      return next(createError(500, `Error updating Induction for prisoner ${prisonNumber}. Error: ${e}`))
+    }
+  }
+
+  private updatedInductionDtoWithPersonalInterests(
+    inductionDto: InductionDto,
+    personalInterestsForm: PersonalInterestsForm,
+  ): InductionDto {
+    const updatedInterests: PersonalInterestDto[] = personalInterestsForm.personalInterests.map(interest => {
+      return {
+        interestType: interest,
+        interestTypeOther:
+          interest === PersonalInterestsValue.OTHER ? personalInterestsForm.personalInterestsOther : undefined,
+      }
+    })
+    return {
+      ...inductionDto,
+      personalSkillsAndInterests: {
+        ...inductionDto.personalSkillsAndInterests,
+        interests: updatedInterests,
+      },
+    }
+  }
 }
