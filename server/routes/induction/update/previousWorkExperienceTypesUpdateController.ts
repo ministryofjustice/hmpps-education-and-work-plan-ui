692813aa506c596f0689e50ba632d84fbb85c9b2
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-03-04 09:25:38 +0000

    RR-646 - Tidying of private methods

--- server/routes/induction/update/previousWorkExperienceTypesUpdateController.ts
+++ server/routes/induction/update/previousWorkExperienceTypesUpdateController.ts
@@ -51,10 +51,10 @@ export default class PreviousWorkExperienceTypesUpdateController extends Previou
     }
 
     if (
-      this.noChangesToPreviousWorkExperienceSubmitted(inductionDto, previousWorkExperienceTypesForm) ||
-      this.onlyRemovalsWereMadeToPreviousWorkExperienceSubmitted(inductionDto, previousWorkExperienceTypesForm)
+      noChangesToPreviousWorkExperienceSubmitted(inductionDto, previousWorkExperienceTypesForm) ||
+      onlyRemovalsWereMadeToPreviousWorkExperienceSubmitted(inductionDto, previousWorkExperienceTypesForm)
     ) {
-      if (this.onlyRemovalsWereMadeToPreviousWorkExperienceSubmitted(inductionDto, previousWorkExperienceTypesForm)) {
+      if (onlyRemovalsWereMadeToPreviousWorkExperienceSubmitted(inductionDto, previousWorkExperienceTypesForm)) {
         logger.debug(
           'Previous Work Experiences changes are only removed types so Induction can be updated without starting a flow',
         )
@@ -78,12 +78,12 @@ export default class PreviousWorkExperienceTypesUpdateController extends Previou
          - any additional job types that have been added by the form submission
          - and OTHER if it's value has changed
      */
-    const typesOfPreviousWorkExperienceToShowDetailsFormFor = [
-      ...this.typesOfPreviousWorkExperienceToShowDetailsFormFor(inductionDto, previousWorkExperienceTypesForm),
+    const workExperienceTypesToShowDetailsFormFor = [
+      ...typesOfPreviousWorkExperienceToShowDetailsFormFor(inductionDto, previousWorkExperienceTypesForm),
     ].sort(previousWorkExperienceTypeScreenOrderComparator) // sort them by the order presented on screen (which is not alphabetic on the enum values)
 
     logger.debug(
-      `Previous Work Experiences changes resulting in going to the Detail pages for ${typesOfPreviousWorkExperienceToShowDetailsFormFor}`,
+      `Previous Work Experiences changes resulting in going to the Detail pages for ${workExperienceTypesToShowDetailsFormFor}`,
     )
 
     // TODO - replace with correct redirect once we've implemented form redirect/handling for a "queue" of Previous Work Experience Detail pages
@@ -100,7 +100,7 @@ export default class PreviousWorkExperienceTypesUpdateController extends Previou
     req: Request,
     prisonerSummary: PrisonerSummary,
   ) => {
-    const updatedInduction = this.updatedInductionDtoWithRemovedPreviousWorkExperiencesAndChangesToOther(
+    const updatedInduction = updatedInductionDtoWithRemovedPreviousWorkExperiencesAndChangesToOther(
       inductionDto,
       previousWorkExperienceTypesForm,
     )
@@ -115,149 +115,137 @@ export default class PreviousWorkExperienceTypesUpdateController extends Previou
       throw e
     }
   }
+}
 
-  /**
-   * Returns an [InductionDto] that represents the Induction with changes made to it.
-   * Specifically this method is only for use when
-   *   * Previous Work Experiences have been removed
-   *   * Or the value for Other has been changed
-   *
-   * This method is not suitable for use when Previous Work Experiences have been added.
-   */
-  private updatedInductionDtoWithRemovedPreviousWorkExperiencesAndChangesToOther(
-    inductionDto: InductionDto,
-    previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
-  ): InductionDto {
-    const removedPreviousWorkExperienceTypes = this.removedPreviousWorkExperienceTypes(
-      inductionDto,
-      previousWorkExperienceTypesForm,
-    )
+/**
+ * Returns an [InductionDto] that represents the Induction with changes made to it.
+ * Specifically this method is only for use when
+ *   * Previous Work Experiences have been removed
+ *   * Or the value for Other has been changed
+ *
+ * This method is not suitable for use when Previous Work Experiences have been added.
+ */
+const updatedInductionDtoWithRemovedPreviousWorkExperiencesAndChangesToOther = (
+  inductionDto: InductionDto,
+  previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
+): InductionDto => {
+  const removedTypes = removedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm)
 
-    const updatedPreviousWorkExperiences: Array<PreviousWorkExperienceDto> =
-      inductionDto.previousWorkExperiences.experiences
-        .filter(experience => !removedPreviousWorkExperienceTypes.includes(experience.experienceType))
-        // We have filtered out all the work experiences that have been removed
-        .map(experience => {
-          if (experience.experienceType === TypeOfWorkExperienceValue.OTHER) {
-            return {
-              ...experience,
-              experienceTypeOther: previousWorkExperienceTypesForm.typeOfWorkExperienceOther,
-            }
-          }
+  const updatedPreviousWorkExperiences: Array<PreviousWorkExperienceDto> =
+    inductionDto.previousWorkExperiences.experiences
+      .filter(experience => !removedTypes.includes(experience.experienceType))
+      // We have filtered out all the work experiences that have been removed
+      .map(experience => {
+        if (experience.experienceType === TypeOfWorkExperienceValue.OTHER) {
           return {
             ...experience,
+            experienceTypeOther: previousWorkExperienceTypesForm.typeOfWorkExperienceOther,
           }
-        })
+        }
+        return {
+          ...experience,
+        }
+      })
 
-    return {
-      ...inductionDto,
-      previousWorkExperiences: {
-        ...inductionDto.previousWorkExperiences,
-        experiences: updatedPreviousWorkExperiences,
-      },
-    }
+  return {
+    ...inductionDto,
+    previousWorkExperiences: {
+      ...inductionDto.previousWorkExperiences,
+      experiences: updatedPreviousWorkExperiences,
+    },
   }
+}
 
-  /**
-   * Returns the list of [TypeOfWorkExperienceValue] for the Previous Work Experiences on the current induction
-   */
-  private previousWorkExperienceTypesOnCurrentInduction = (
-    inductionDto: InductionDto,
-  ): Array<TypeOfWorkExperienceValue> =>
-    inductionDto.previousWorkExperiences.experiences.map(experience => experience.experienceType)
+/**
+ * Returns the list of [TypeOfWorkExperienceValue] for the Previous Work Experiences on the current induction
+ */
+const previousWorkExperienceTypesOnCurrentInduction = (inductionDto: InductionDto): Array<TypeOfWorkExperienceValue> =>
+  inductionDto.previousWorkExperiences.experiences.map(experience => experience.experienceType)
 
-  /**
-   * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns a list of [TypeOfWorkExperienceValue]
-   * that represent the Previous Work Experiences that are to be removed.
-   * IE. those that exist on the Induction, but not in the [PreviousWorkExperienceTypesForm]
-   */
-  private removedPreviousWorkExperienceTypes = (
-    inductionDto: InductionDto,
-    previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
-  ): Array<TypeOfWorkExperienceValue> =>
-    [...this.previousWorkExperienceTypesOnCurrentInduction(inductionDto)].filter(
-      type => !previousWorkExperienceTypesForm.typeOfWorkExperience.includes(type),
-    )
+/**
+ * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns a list of [TypeOfWorkExperienceValue]
+ * that represent the Previous Work Experiences that are to be removed.
+ * IE. those that exist on the Induction, but not in the [PreviousWorkExperienceTypesForm]
+ */
+const removedPreviousWorkExperienceTypes = (
+  inductionDto: InductionDto,
+  previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
+): Array<TypeOfWorkExperienceValue> =>
+  [...previousWorkExperienceTypesOnCurrentInduction(inductionDto)].filter(
+    type => !previousWorkExperienceTypesForm.typeOfWorkExperience.includes(type),
+  )
 
-  /**
-   * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns a list of [TypeOfWorkExperienceValue]
-   * that represent the Previous Work Experiences that are to be added.
-   * IE. those that do not exist on the Induction, but are in the [PreviousWorkExperienceTypesForm]
-   */
-  private addedPreviousWorkExperienceTypes = (
-    inductionDto: InductionDto,
-    previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
-  ): Array<TypeOfWorkExperienceValue> =>
-    [...previousWorkExperienceTypesForm.typeOfWorkExperience].filter(
-      type => !this.previousWorkExperienceTypesOnCurrentInduction(inductionDto).includes(type),
-    )
+/**
+ * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns a list of [TypeOfWorkExperienceValue]
+ * that represent the Previous Work Experiences that are to be added.
+ * IE. those that do not exist on the Induction, but are in the [PreviousWorkExperienceTypesForm]
+ */
+const addedPreviousWorkExperienceTypes = (
+  inductionDto: InductionDto,
+  previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
+): Array<TypeOfWorkExperienceValue> =>
+  [...previousWorkExperienceTypesForm.typeOfWorkExperience].filter(
+    type => !previousWorkExperienceTypesOnCurrentInduction(inductionDto).includes(type),
+  )
 
-  /**
-   * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns `true` if both the Induction and the
-   * [PreviousWorkExperienceTypesForm] inclyde a Previous Work Experience type of OTHER, and the it's value is different.
-   */
-  private otherTypeOfWorkExperienceHasBeenChanged = (
-    inductionDto: InductionDto,
-    previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
-  ): boolean =>
-    this.previousWorkExperienceTypesOnCurrentInduction(inductionDto).includes(TypeOfWorkExperienceValue.OTHER) &&
-    previousWorkExperienceTypesForm.typeOfWorkExperience.includes(TypeOfWorkExperienceValue.OTHER) &&
-    inductionDto.previousWorkExperiences.experiences.find(
-      experience => experience.experienceType === TypeOfWorkExperienceValue.OTHER,
-    )?.experienceTypeOther !== previousWorkExperienceTypesForm.typeOfWorkExperienceOther
+/**
+ * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns `true` if both the Induction and the
+ * [PreviousWorkExperienceTypesForm] inclyde a Previous Work Experience type of OTHER, and the it's value is different.
+ */
+const otherTypeOfWorkExperienceHasBeenChanged = (
+  inductionDto: InductionDto,
+  previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
+): boolean =>
+  previousWorkExperienceTypesOnCurrentInduction(inductionDto).includes(TypeOfWorkExperienceValue.OTHER) &&
+  previousWorkExperienceTypesForm.typeOfWorkExperience.includes(TypeOfWorkExperienceValue.OTHER) &&
+  inductionDto.previousWorkExperiences.experiences.find(
+    experience => experience.experienceType === TypeOfWorkExperienceValue.OTHER,
+  )?.experienceTypeOther !== previousWorkExperienceTypesForm.typeOfWorkExperienceOther
 
-  /**
-   * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns `true` if there have been no changes
-   * to the Previous Work Experiences.
-   * IE. No removals, no additions, and OTHER has not been changed.
-   */
-  private noChangesToPreviousWorkExperienceSubmitted = (
-    inductionDto: InductionDto,
-    previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
-  ): boolean =>
-    this.removedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm).length === 0 &&
-    this.addedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm).length === 0 &&
-    !this.otherTypeOfWorkExperienceHasBeenChanged(inductionDto, previousWorkExperienceTypesForm)
+/**
+ * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns `true` if there have been no changes
+ * to the Previous Work Experiences.
+ * IE. No removals, no additions, and OTHER has not been changed.
+ */
+const noChangesToPreviousWorkExperienceSubmitted = (
+  inductionDto: InductionDto,
+  previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
+): boolean =>
+  removedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm).length === 0 &&
+  addedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm).length === 0 &&
+  !otherTypeOfWorkExperienceHasBeenChanged(inductionDto, previousWorkExperienceTypesForm)
 
-  /**
-   * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns `true` if the only changes to the
-   * Previous Work Experiences are the removal of one or more Previous Work Experience types.
-   * IE. One or more removals, no additions, and OTHER has not been changed.
-   */
-  private onlyRemovalsWereMadeToPreviousWorkExperienceSubmitted = (
-    inductionDto: InductionDto,
-    previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
-  ): boolean =>
-    this.removedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm).length > 0 &&
-    this.addedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm).length === 0 &&
-    !this.otherTypeOfWorkExperienceHasBeenChanged(inductionDto, previousWorkExperienceTypesForm)
+/**
+ * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns `true` if the only changes to the
+ * Previous Work Experiences are the removal of one or more Previous Work Experience types.
+ * IE. One or more removals, no additions, and OTHER has not been changed.
+ */
+const onlyRemovalsWereMadeToPreviousWorkExperienceSubmitted = (
+  inductionDto: InductionDto,
+  previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
+): boolean =>
+  removedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm).length > 0 &&
+  addedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm).length === 0 &&
+  !otherTypeOfWorkExperienceHasBeenChanged(inductionDto, previousWorkExperienceTypesForm)
 
-  /**
-   * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns a list of [TypeOfWorkExperienceValue]
-   * that represent the Previous Work Experiences that will remain on the Induction after any removals, plus any additions
-   * from the form; plus OTHER if it's value has changed.
-   */
-
-  private typesOfPreviousWorkExperienceToShowDetailsFormFor = (
-    inductionDto: InductionDto,
-    previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
-  ): Array<TypeOfWorkExperienceValue> => {
-    const removedPreviousWorkExperienceTypes = this.removedPreviousWorkExperienceTypes(
-      inductionDto,
-      previousWorkExperienceTypesForm,
-    )
-    const addedPreviousWorkExperienceTypes = this.addedPreviousWorkExperienceTypes(
-      inductionDto,
-      previousWorkExperienceTypesForm,
-    )
-    return [
-      ...this.previousWorkExperienceTypesOnCurrentInduction(inductionDto), // All work types that were on the induction to start with
-      ...addedPreviousWorkExperienceTypes, // plus any that have been added on this form
-      this.otherTypeOfWorkExperienceHasBeenChanged(inductionDto, previousWorkExperienceTypesForm) // plus OTHER if the value has changed
-        ? TypeOfWorkExperienceValue.OTHER
-        : undefined,
-    ]
-      .filter(type => !removedPreviousWorkExperienceTypes.includes(type)) // filter out any that were removed on the form
-      .filter(type => type !== undefined) // filter out any undefined values in the array
-  }
+/**
+ * Given the current Induction and the [PreviousWorkExperienceTypesForm], returns a list of [TypeOfWorkExperienceValue]
+ * that represent the Previous Work Experiences that will remain on the Induction after any removals, plus any additions
+ * from the form; plus OTHER if it's value has changed.
+ */
+const typesOfPreviousWorkExperienceToShowDetailsFormFor = (
+  inductionDto: InductionDto,
+  previousWorkExperienceTypesForm: PreviousWorkExperienceTypesForm,
+): Array<TypeOfWorkExperienceValue> => {
+  const removedTypes = removedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm)
+  const addedTypes = addedPreviousWorkExperienceTypes(inductionDto, previousWorkExperienceTypesForm)
+  return [
+    ...previousWorkExperienceTypesOnCurrentInduction(inductionDto), // All work types that were on the induction to start with
+    ...addedTypes, // plus any that have been added on this form
+    otherTypeOfWorkExperienceHasBeenChanged(inductionDto, previousWorkExperienceTypesForm) // plus OTHER if the value has changed
+      ? TypeOfWorkExperienceValue.OTHER
+      : undefined,
+  ]
+    .filter(type => !removedTypes.includes(type)) // filter out any that were removed on the form
+    .filter(type => type !== undefined) // filter out any undefined values in the array
 }
