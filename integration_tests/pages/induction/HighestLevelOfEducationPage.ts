b15fb24233c343e499cd17a67219d000bc106453
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2024-03-11 17:16:24 +0000

    RR-694 - Migrated nunjucks view for Highest Level of Education (#481)

---
+++ integration_tests/pages/induction/HighestLevelOfEducationPage.ts
@@ -0,0 +1,10 @@
+import Page from '../page'
+
+/**
+ * Cypress page class representing the Induction "Highest Level of Education" page
+ */
+export default class HighestLevelOfEducationPage extends Page {
+  constructor() {
+    super('induction-highest-level-of-education')
+  }
+}
