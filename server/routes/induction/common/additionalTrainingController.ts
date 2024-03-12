fc9ec7cce81a7483cc69fe2b30bcd0eeae69fd1c
Matt Wills <matt.wills@digital.justice.gov.uk>
2024-03-12 15:33:23 +0000

    RR-698 - Functionality to update a Prisoner's additional training

---
+++ server/routes/induction/common/additionalTrainingController.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+import type { InductionDto } from 'inductionDto'
+import type { AdditionalTrainingForm } from 'inductionForms'
+import InductionController from './inductionController'
+import AdditionalTrainingView from './additionalTrainingView'
+
+/**
+ * Abstract controller class defining functionality common to both the Create and Update Induction journeys.
+ */
+export default abstract class AdditionalTrainingController extends InductionController {
+  /**
+   * Returns the Additional Training view; suitable for use by the Create and Update journeys.
+   */
+  getAdditionalTrainingView: RequestHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    const { prisonerSummary, inductionDto } = req.session
+
+    const additionalTrainingForm = req.session.additionalTrainingForm || toAdditionalTrainingForm(inductionDto)
+    req.session.additionalTrainingForm = undefined
+
+    const view = new AdditionalTrainingView(
+      prisonerSummary,
+      this.getBackLinkUrl(req),
+      this.getBackLinkAriaText(req),
+      additionalTrainingForm,
+      req.flash('errors'),
+    )
+    return res.render('pages/induction/additionalTraining/index', { ...view.renderArgs })
+  }
+}
+
+const toAdditionalTrainingForm = (inductionDto: InductionDto): AdditionalTrainingForm => {
+  return {
+    additionalTraining: inductionDto.previousTraining.trainingTypes,
+    additionalTrainingOther: inductionDto.previousTraining.trainingTypeOther,
+  }
+}
