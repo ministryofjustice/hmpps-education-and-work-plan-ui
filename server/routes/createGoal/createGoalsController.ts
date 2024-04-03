9f2ed35685b2d304cb66c5340778e7c297e5e5db
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-04-03 10:17:36 +0100

    RR-748 - Fix routes and basic controller with tests

--- server/routes/createGoal/createGoalsController.ts
+++ server/routes/createGoal/createGoalsController.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from 'express'
-import moment from 'moment'
-import type { CreateGoalsForm } from 'forms'
+import { UTCDate } from '@date-fns/utc'
+import { startOfDay } from 'date-fns'
 import CreateGoalsView from './createGoalsView'
 import futureGoalTargetDateCalculator from '../futureGoalTargetDateCalculator'
 import validateCreateGoalsForm from './createGoalsFormValidator'
@@ -12,44 +12,37 @@ export default class CreateGoalsController {
     const { prisonNumber } = req.params
     const { prisonerSummary } = req.session
 
-    req.session.createGoalsForm = req.session.createGoalsForm || []
-
-    if (!req.session.createGoalsForm) {
-      req.session.createGoalsForm = {
-        prisonNumber,
-        goals: [
-          {
-            steps: [{ title: '' }],
-          },
-        ],
-      } as CreateGoalsForm
+    const createGoalsForm = req.session.createGoalsForm || {
+      prisonNumber,
+      goals: [
+        {
+          steps: [],
+        },
+      ],
     }
+    req.session.createGoalsForm = undefined
 
-    const today = moment().toDate()
+    const today = startOfDay(new UTCDate())
     const futureGoalTargetDates = [
       futureGoalTargetDateCalculator(today, 3),
       futureGoalTargetDateCalculator(today, 6),
       futureGoalTargetDateCalculator(today, 12),
     ]
 
-    const view = new CreateGoalsView(
-      prisonerSummary,
-      req.session.createGoalsForm,
-      futureGoalTargetDates,
-      req.flash('errors'),
-    )
+    const view = new CreateGoalsView(prisonerSummary, createGoalsForm, futureGoalTargetDates, req.flash('errors'))
     return res.render('pages/createGoals/index', { ...view.renderArgs })
   }
 
   // TODO: RR-748 - Implement submit handler for new create goal journey
   submitCreateGoalsForm: RequestHandler = async (req, res, next): Promise<void> => {
     const { prisonNumber } = req.params
-    const { createGoalsForm } = req.session.createGoalsForm
+    req.session.createGoalsForm = { ...req.body }
+    const { createGoalsForm } = req.session
 
     const errors = validateCreateGoalsForm(createGoalsForm)
     if (errors.length > 0) {
       req.flash('errors', errors)
-      return res.redirect(`/plan/${prisonNumber}/goals/1/create`)
+      return res.redirect(`/plan/${prisonNumber}/goals/create`)
     }
 
     return res.redirect(`/plan/${prisonNumber}/view/overview`)
