98eae0cfba21685e04c976bb65b25034df5ca294
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2024-04-04 14:17:02 +0100

    RR-736 - Populate Check Your Answers page with values (#548)

--- server/routes/induction/common/checkYourAnswersController.ts
+++ server/routes/induction/common/checkYourAnswersController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 import InductionController from './inductionController'
-import CheckYourAnswersView from './additionalTrainingView'
+import CheckYourAnswersView from './checkYourAnswersView'
 
 /**
  * Abstract controller class defining functionality common to both the Create and Update Induction journeys.
@@ -12,12 +12,7 @@ export default abstract class CheckYourAnswersController extends InductionContro
   getCheckYourAnswersView: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { prisonerSummary, inductionDto } = req.session
 
-    const view = new CheckYourAnswersView(
-      prisonerSummary,
-      this.getBackLinkUrl(req),
-      this.getBackLinkAriaText(req),
-      inductionDto,
-    )
+    const view = new CheckYourAnswersView(prisonerSummary, inductionDto)
     return res.render('pages/induction/checkYourAnswers/index', { ...view.renderArgs })
   }
 }
