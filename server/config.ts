439595612178109452f402bb5ede90bd9fe76ece
Lee Jacobson <125488090+leejacobson-moj@users.noreply.github.com>
2024-04-08 10:18:50 +0100

    Merged template health check endpoint updates (#552)

--- server/config.ts
+++ server/config.ts
@@ -17,6 +17,7 @@ function get<T>(name: string, fallback: T, options = { requireInProduction: fals
 const requiredInProduction = { requireInProduction: true }
 
 export class AgentConfig {
+  // Sets the working socket to timeout after timeout milliseconds of inactivity on the working socket.
   timeout: number
 
   constructor(timeout = 8000) {
@@ -27,7 +28,11 @@ export class AgentConfig {
 export interface ApiConfig {
   url: string
   timeout: {
+    // sets maximum time to wait for the first byte to arrive from the server, but it does not limit how long the
+    // entire download can take.
     response: number
+    // sets a deadline for the entire request (including all uploads, redirects, server processing time) to complete.
+    // If the response isn't fully downloaded within that time, the request will be aborted.
     deadline: number
   }
   agent: AgentConfig
@@ -49,7 +54,9 @@ const auditConfig = () => {
 
 export default {
   buildNumber: get('BUILD_NUMBER', '1_0_0', requiredInProduction),
+  productId: get('PRODUCT_ID', 'UNASSIGNED', requiredInProduction),
   gitRef: get('GIT_REF', 'xxxxxxxxxxxxxxxxxxx', requiredInProduction),
+  branchName: get('GIT_BRANCH', 'xxxxxxxxxxxxxxxxxxx', requiredInProduction),
   production,
   https: production,
   staticResourceCacheDuration: '1h',
