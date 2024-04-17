87bc77e894aaff72c16c4b30dd60469447a32a3f
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-04-17 08:40:28 +0100

    RR-736 - Created cypress tasks to reduce duplication and simplify tests

--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -1,5 +1,32 @@
 import { RequestPatternBuilder } from '../mockApis/wiremock/requestPatternBuilder'
 import { verify } from '../mockApis/wiremock'
+import Page from '../pages/page'
+import HopingToWorkOnReleasePage from '../pages/induction/HopingToWorkOnReleasePage'
+import HopingToGetWorkValue from '../../server/enums/hopingToGetWorkValue'
+import ReasonsNotToGetWorkPage from '../pages/induction/ReasonsNotToGetWorkPage'
+import ReasonNotToGetWorkValue from '../../server/enums/reasonNotToGetWorkValue'
+import QualificationsListPage from '../pages/induction/QualificationsListPage'
+import AdditionalTrainingPage from '../pages/induction/AdditionalTrainingPage'
+import InPrisonWorkPage from '../pages/induction/InPrisonWorkPage'
+import InPrisonWorkValue from '../../server/enums/inPrisonWorkValue'
+import InPrisonTrainingPage from '../pages/induction/InPrisonTrainingPage'
+import InPrisonTrainingValue from '../../server/enums/inPrisonTrainingValue'
+import CheckYourAnswersPage from '../pages/induction/CheckYourAnswersPage'
+import WorkedBeforePage from '../pages/induction/WorkedBeforePage'
+import YesNoValue from '../../server/enums/yesNoValue'
+import PreviousWorkExperienceTypesPage from '../pages/induction/PreviousWorkExperienceTypesPage'
+import TypeOfWorkExperienceValue from '../../server/enums/typeOfWorkExperienceValue'
+import PreviousWorkExperienceDetailPage from '../pages/induction/PreviousWorkExperienceDetailPage'
+import FutureWorkInterestTypesPage from '../pages/induction/FutureWorkInterestTypesPage'
+import WorkInterestTypeValue from '../../server/enums/workInterestTypeValue'
+import FutureWorkInterestRolesPage from '../pages/induction/FutureWorkInterestRolesPage'
+import SkillsPage from '../pages/induction/SkillsPage'
+import SkillsValue from '../../server/enums/skillsValue'
+import PersonalInterestsPage from '../pages/induction/PersonalInterestsPage'
+import PersonalInterestsValue from '../../server/enums/personalInterestsValue'
+import AffectAbilityToWorkPage from '../pages/induction/AffectAbilityToWorkPage'
+import AbilityToWorkValue from '../../server/enums/abilityToWorkValue'
+import PrisonerListPage from '../pages/prisonerList/PrisonerListPage'
 
 Cypress.Commands.add('signIn', (options = { failOnStatusCode: false }) => {
   cy.request('/')
@@ -13,3 +40,110 @@ Cypress.Commands.add('wiremockVerify', (requestPatternBuilder: RequestPatternBui
 Cypress.Commands.add('wiremockVerifyNoInteractions', (requestPatternBuilder: RequestPatternBuilder) => {
   return cy.wrap(verify(0, requestPatternBuilder)).should('be.true')
 })
+
+Cypress.Commands.add('signInAsUserWithViewAuthorityToArriveOnPrisonerListPage', () => {
+  signInWithAuthority('VIEW')
+})
+
+Cypress.Commands.add('signInAsUserWithEditAuthorityToArriveOnPrisonerListPage', () => {
+  signInWithAuthority('EDIT')
+})
+
+Cypress.Commands.add('updateShortQuestionSetInductionToArriveOnCheckYourAnswers', (prisonNumber = 'G6115VJ') => {
+  /* Update a Long Question Set Induction by answering the Do They Want To Work On Release question to NO to turn it
+   * into a Short Question Set Induction. Answer all the questions to get to the Check Your Answers page.
+   */
+  cy.task('stubGetInductionLongQuestionSet') // Long question set Induction with Hoping to work on release as YES
+  cy.visit(`/prisoners/${prisonNumber}/induction/hoping-to-work-on-release`)
+  // Hoping To Work On Release is the first page
+  Page.verifyOnPage(HopingToWorkOnReleasePage) //
+    .selectHopingWorkOnRelease(HopingToGetWorkValue.NO)
+    .submitPage()
+  // Reasons Not To Work is the next page, and is only asked on the short question set, so will not have any previous answer from the original long question set Induction
+  Page.verifyOnPage(ReasonsNotToGetWorkPage) //
+    .chooseReasonNotToGetWork(ReasonNotToGetWorkValue.HEALTH)
+    .submitPage()
+  // Qualifications List is the next page. Qualifications are asked on the long question set, so this will already have qualifications set
+  Page.verifyOnPage(QualificationsListPage) //
+    .submitPage()
+  // Additional Training is the next page. This is asked on the long question set, so this will already have answers set
+  Page.verifyOnPage(AdditionalTrainingPage) //
+    .submitPage()
+  // In Prison Work Interests is the next page, and is only asked on the short question set, so will not have any previous answer from the original long question set Induction
+  Page.verifyOnPage(InPrisonWorkPage) //
+    .chooseWorkType(InPrisonWorkValue.CLEANING_AND_HYGIENE)
+    .submitPage()
+  // In Prison Training Interests is the next page, and is only asked on the short question set, so will not have any previous answer from the original long question set Induction
+  Page.verifyOnPage(InPrisonTrainingPage) //
+    .chooseInPrisonTraining(InPrisonTrainingValue.BARBERING_AND_HAIRDRESSING)
+    .submitPage()
+  // Arrive on Check Your Answers page
+  Page.verifyOnPage(CheckYourAnswersPage)
+})
+
+Cypress.Commands.add('updateLongQuestionSetInductionToArriveOnCheckYourAnswers', (prisonNumber = 'G6115VJ') => {
+  /* Update a Short Question Set Induction by answering the Do They Want To Work On Release question to YES to turn it
+   * into a Long Question Set Induction. Answer all the questions to get to the Check Your Answers page.
+   */
+  cy.task('stubGetInductionShortQuestionSet') // Long question set Induction with Hoping to work on release as YES
+  cy.visit(`/prisoners/${prisonNumber}/induction/hoping-to-work-on-release`)
+  // Hoping To Work On Release is the first page
+  Page.verifyOnPage(HopingToWorkOnReleasePage) //
+    .selectHopingWorkOnRelease(HopingToGetWorkValue.YES)
+    .submitPage()
+  // Qualifications List is the next page. Qualifications are asked on the short question set, so this will already have qualifications set
+  Page.verifyOnPage(QualificationsListPage) //
+    .submitPage()
+  // Additional Training is the next page. This is asked on the short question set, so this will already have answers set
+  Page.verifyOnPage(AdditionalTrainingPage) //
+    .submitPage()
+  // 'Has the prisoner worked before' is the next page. This is not asked on the short question set.
+  // Answer 'Yes' to create an Induction that has details of the prisoners previous work experience.
+  Page.verifyOnPage(WorkedBeforePage) //
+    .selectWorkedBefore(YesNoValue.YES)
+    .submitPage()
+  // Preview Work Experience types is the next page. This is not asked on the short question set.
+  Page.verifyOnPage(PreviousWorkExperienceTypesPage) //
+    .choosePreviousWorkExperience(TypeOfWorkExperienceValue.OFFICE)
+    .submitPage()
+  Page.verifyOnPage(PreviousWorkExperienceDetailPage) //
+    .setJobRole('Office junior')
+    .setJobDetails('Filing and photocopying')
+    .submitPage()
+  // Work Interests page is the next page. This is not asked on the short question set.
+  Page.verifyOnPage(FutureWorkInterestTypesPage) //
+    .chooseWorkInterestType(WorkInterestTypeValue.DRIVING)
+    .submitPage()
+  Page.verifyOnPage(FutureWorkInterestRolesPage) //
+    .setWorkInterestRole(WorkInterestTypeValue.DRIVING, 'Driving instructor')
+    .submitPage()
+  // Personal skills page is the next page. This is not asked on the short question set.
+  Page.verifyOnPage(SkillsPage) //
+    .chooseSkill(SkillsValue.TEAMWORK)
+    .submitPage()
+  // Personal Interests is the next page. This is not asked on the short question set.
+  Page.verifyOnPage(PersonalInterestsPage) //
+    .choosePersonalInterest(PersonalInterestsValue.SOCIAL)
+    .submitPage()
+  // Factors Affecting Ability To Work is the next page. This is not asked on the short question set.
+  Page.verifyOnPage(AffectAbilityToWorkPage) //
+    .chooseAffectAbilityToWork(AbilityToWorkValue.HEALTH_ISSUES)
+    .submitPage()
+  // Arrive on Check Your Answers page
+  Page.verifyOnPage(CheckYourAnswersPage)
+})
+
+const signInWithAuthority = (authority: 'EDIT' | 'VIEW') => {
+  cy.task('reset')
+  cy.task(authority === 'EDIT' ? 'stubSignInAsUserWithEditAuthority' : 'stubSignInAsUserWithViewAuthority')
+  cy.task('stubAuthUser')
+  cy.task('stubGetHeaderComponent')
+  cy.task('stubGetFooterComponent')
+  cy.task('stubPrisonerList')
+  cy.task('stubCiagInductionList')
+  cy.task('stubActionPlansList')
+  cy.task('getPrisonerById')
+  cy.task('stubLearnerProfile')
+  cy.signIn()
+  Page.verifyOnPage(PrisonerListPage)
+}
