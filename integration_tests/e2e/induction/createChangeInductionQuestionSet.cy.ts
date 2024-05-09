bf94254e37e371cef3fcfc26e22bc43b90c5f774
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-05-09 09:03:25 +0100

    RR-782 - Change question set from Check Your Answers from a short to long question set induction

--- integration_tests/e2e/induction/createChangeInductionQuestionSet.cy.ts
+++ integration_tests/e2e/induction/createChangeInductionQuestionSet.cy.ts
@@ -20,6 +20,20 @@ import CreateGoalsPage from '../../pages/goal/CreateGoalsPage'
 import { postRequestedFor } from '../../mockApis/wiremock/requestPatternBuilder'
 import { urlEqualTo } from '../../mockApis/wiremock/matchers/url'
 import { matchingJsonPath } from '../../mockApis/wiremock/matchers/content'
+import WorkedBeforePage from '../../pages/induction/WorkedBeforePage'
+import YesNoValue from '../../../server/enums/yesNoValue'
+import PreviousWorkExperienceTypesPage from '../../pages/induction/PreviousWorkExperienceTypesPage'
+import TypeOfWorkExperienceValue from '../../../server/enums/typeOfWorkExperienceValue'
+import PreviousWorkExperienceDetailPage from '../../pages/induction/PreviousWorkExperienceDetailPage'
+import FutureWorkInterestTypesPage from '../../pages/induction/FutureWorkInterestTypesPage'
+import WorkInterestTypeValue from '../../../server/enums/workInterestTypeValue'
+import FutureWorkInterestRolesPage from '../../pages/induction/FutureWorkInterestRolesPage'
+import SkillsPage from '../../pages/induction/SkillsPage'
+import SkillsValue from '../../../server/enums/skillsValue'
+import PersonalInterestsPage from '../../pages/induction/PersonalInterestsPage'
+import PersonalInterestsValue from '../../../server/enums/personalInterestsValue'
+import AffectAbilityToWorkPage from '../../pages/induction/AffectAbilityToWorkPage'
+import AbilityToWorkValue from '../../../server/enums/abilityToWorkValue'
 
 context(`Change new Induction question set by updating 'Hoping to work on release' from Check Your Answers`, () => {
   const prisonNumberForPrisonerWithNoInduction = 'A00001A'
@@ -110,6 +124,143 @@ context(`Change new Induction question set by updating 'Hoping to work on releas
               "@.inPrisonInterests.inPrisonWorkInterests[0].workType == 'CLEANING_AND_HYGIENE' && !@.inPrisonInterests.inPrisonWorkInterests[0].workTypeOther && " +
               '@.inPrisonInterests.inPrisonTrainingInterests.size() == 1 && ' +
               "@.inPrisonInterests.inPrisonTrainingInterests[0].trainingType == 'BARBERING_AND_HAIRDRESSING' && !@.inPrisonInterests.inPrisonTrainingInterests[0].trainingTypeOther)]",
+          ),
+        ),
+    )
+  })
+
+  it(`should change a short question set Induction into a long question set Induction given 'Hoping to work on release' is changed to Yes from Check Your Answers`, () => {
+    // Given
+    cy.createShortQuestionSetInductionToArriveOnCheckYourAnswers(prisonNumberForPrisonerWithNoInduction)
+    Page.verifyOnPage(CheckYourAnswersPage)
+
+    // When
+    Page.verifyOnPage(CheckYourAnswersPage) //
+      .clickHopingToWorkOnReleaseChangeLink()
+      .selectHopingWorkOnRelease(HopingToGetWorkValue.YES)
+      .submitPage()
+
+    // Qualifications List is the next page. Qualifications are asked on the short question set, so this will already have qualifications set
+    // Add a new qualification; just to test going through each page in the flow
+    Page.verifyOnPage(QualificationsListPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/hoping-to-work-on-release`)
+      .clickToAddAnotherQualification()
+      .selectQualificationLevel(QualificationLevelValue.LEVEL_4)
+      .submitPage()
+    Page.verifyOnPage(QualificationDetailsPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/qualification-level`)
+      .setQualificationSubject('Spanish')
+      .setQualificationGrade('Distinction')
+      .submitPage()
+    Page.verifyOnPage(QualificationsListPage) //
+      .submitPage()
+
+    // Additional Training is the next page. This is asked on the short question set, so this will already have answers set
+    Page.verifyOnPage(AdditionalTrainingPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/qualifications`)
+      .submitPage()
+
+    // 'Has the prisoner worked before' is the next page. This is not asked on the short question set.
+    // Answer 'Yes' to test going through the subsequent pages that ask about previous work experience.
+    Page.verifyOnPage(WorkedBeforePage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/additional-training`)
+      .selectWorkedBefore(YesNoValue.YES)
+      .submitPage()
+
+    // Preview Work Experience types is the next page. This is not asked on the short question set.
+    // Select 2 previous work experience types as that will cause the next page (work experience detail) to be displayed twice.
+    Page.verifyOnPage(PreviousWorkExperienceTypesPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/has-worked-before`)
+      .choosePreviousWorkExperience(TypeOfWorkExperienceValue.TECHNICAL)
+      .choosePreviousWorkExperience(TypeOfWorkExperienceValue.OFFICE)
+      .submitPage()
+    Page.verifyOnPage(PreviousWorkExperienceDetailPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/previous-work-experience`)
+      .setJobRole('Software developer')
+      .setJobDetails('Designing, developing and testing software: Dec 2009 - Aug 2020')
+      .submitPage()
+    Page.verifyOnPage(PreviousWorkExperienceDetailPage) //
+      .hasBackLinkTo(
+        `/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/previous-work-experience/technical`,
+      )
+      .setJobRole('Office junior')
+      .setJobDetails('Filing and photocopying: Sept 2000 - Dec 2009')
+      .submitPage()
+
+    // Work Interests page is the next page. This is not asked on the short question set.
+    Page.verifyOnPage(FutureWorkInterestTypesPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/has-worked-before`)
+      .chooseWorkInterestType(WorkInterestTypeValue.CONSTRUCTION)
+      .chooseWorkInterestType(WorkInterestTypeValue.DRIVING)
+      .submitPage()
+    Page.verifyOnPage(FutureWorkInterestRolesPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/work-interest-types`)
+      .setWorkInterestRole(WorkInterestTypeValue.CONSTRUCTION, 'General builder')
+      .setWorkInterestRole(WorkInterestTypeValue.DRIVING, 'Driving instructor')
+      .submitPage()
+
+    // Personal skills page is the next page. This is not asked on the short question set.
+    Page.verifyOnPage(SkillsPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/work-interest-roles`)
+      .chooseSkill(SkillsValue.TEAMWORK)
+      .chooseSkill(SkillsValue.WILLINGNESS_TO_LEARN)
+      .submitPage()
+
+    // Personal Interests is the next page. This is not asked on the short question set.
+    Page.verifyOnPage(PersonalInterestsPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/skills`)
+      .choosePersonalInterest(PersonalInterestsValue.OUTDOOR)
+      .choosePersonalInterest(PersonalInterestsValue.SOCIAL)
+      .submitPage()
+
+    // Factors Affecting Ability To Work is the next page. This is not asked on the short question set.
+    Page.verifyOnPage(AffectAbilityToWorkPage) //
+      .hasBackLinkTo(`/prisoners/${prisonNumberForPrisonerWithNoInduction}/create-induction/personal-interests`)
+      .chooseAffectAbilityToWork(AbilityToWorkValue.HEALTH_ISSUES)
+      .submitPage()
+
+    // Check Your Answers is the final page. Submit the page to save the induction
+    Page.verifyOnPage(CheckYourAnswersPage) //
+      .submitPage()
+
+    // Then
+    Page.verifyOnPage(CreateGoalsPage) //
+    cy.wiremockVerify(
+      postRequestedFor(urlEqualTo(`/inductions/${prisonNumberForPrisonerWithNoInduction}`)) //
+        .withRequestBody(
+          matchingJsonPath(
+            "$[?(@.workOnRelease.hopingToWork == 'YES' && " +
+              '@.previousQualifications.qualifications.size() == 2 && ' +
+              "@.previousQualifications.qualifications[0].subject == 'Computer science' && " +
+              "@.previousQualifications.qualifications[0].grade == 'A*' && " +
+              "@.previousQualifications.qualifications[0].level == 'LEVEL_4' && " +
+              "@.previousQualifications.qualifications[1].subject == 'Spanish' && " +
+              "@.previousQualifications.qualifications[1].grade == 'Distinction' && " +
+              "@.previousQualifications.qualifications[1].level == 'LEVEL_4' && " +
+              '@.previousTraining.trainingTypes.size() == 1 && ' +
+              "@.previousTraining.trainingTypes[0] == 'HGV_LICENCE' && " +
+              '@.previousWorkExperiences.hasWorkedBefore == true && ' +
+              '@.previousWorkExperiences.experiences.size() == 2 && ' +
+              "@.previousWorkExperiences.experiences[0].experienceType == 'TECHNICAL' && " +
+              "@.previousWorkExperiences.experiences[0].role == 'Software developer' && " +
+              "@.previousWorkExperiences.experiences[0].details == 'Designing, developing and testing software: Dec 2009 - Aug 2020' && " +
+              "@.previousWorkExperiences.experiences[1].experienceType == 'OFFICE' && " +
+              "@.previousWorkExperiences.experiences[1].role == 'Office junior' && " +
+              "@.previousWorkExperiences.experiences[1].details == 'Filing and photocopying: Sept 2000 - Dec 2009' && " +
+              '@.futureWorkInterests.interests.size() == 2 && ' +
+              "@.futureWorkInterests.interests[0].workType == 'CONSTRUCTION' && " +
+              "@.futureWorkInterests.interests[0].role == 'General builder' && " +
+              "@.futureWorkInterests.interests[1].workType == 'DRIVING' && " +
+              "@.futureWorkInterests.interests[1].role == 'Driving instructor' && " +
+              '@.personalSkillsAndInterests.skills.size() == 2 && ' +
+              "@.personalSkillsAndInterests.skills[0].skillType == 'TEAMWORK' && " +
+              "@.personalSkillsAndInterests.skills[1].skillType == 'WILLINGNESS_TO_LEARN' && " +
+              '@.personalSkillsAndInterests.interests.size() == 2 && ' +
+              "@.personalSkillsAndInterests.interests[0].interestType == 'OUTDOOR' && " +
+              "@.personalSkillsAndInterests.interests[1].interestType == 'SOCIAL' && " +
+              '@.workOnRelease.affectAbilityToWork.size() == 1 && ' +
+              "@.workOnRelease.affectAbilityToWork[0] == 'HEALTH_ISSUES' && " +
+              "@.workOnRelease.affectAbilityToWorkOther == '')]",
           ),
         ),
     )
