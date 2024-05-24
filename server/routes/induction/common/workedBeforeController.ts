ec7a60e55c5d1c5380eef966819fe7d3e360d56c
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-05-24 08:23:42 +0100

    RR-807 - Fix Back link on Previous Work Experiences Type page

--- server/routes/induction/common/workedBeforeController.ts
+++ server/routes/induction/common/workedBeforeController.ts
@@ -18,7 +18,7 @@ export default abstract class WorkedBeforeController extends InductionController
     this.addCurrentPageToFlowHistoryWhenComingFromCheckYourAnswers(req)
 
     // Check if we are in the midst of changing the main induction question set (in this case from short route to long route)
-    if (req.session.updateInductionQuestionSet) {
+    if (req.session.updateInductionQuestionSet || req.session.pageFlowHistory) {
       this.addCurrentPageToHistory(req)
     }
 
