f1ffbab3037094d3508435dc1726954ecc69a5e9
James Baker <james.baker3@digital.justice.gov.uk>
2024-06-20 12:15:51 +0100

    RR-862: Fix Work has worked before for old short question set induction
    If the induction was performed on the old question set and they answered no to "hoping to work" then the last updated field was not displaying properly for work experience. Now defaults to has worked before last updated and you can also add the work experience if you so choose as you would be able to on a new induction.

--- server/views/pages/overview/partials/workAndInterestsTab/_inductionQuestionSet.test.ts
+++ server/views/pages/overview/partials/workAndInterestsTab/_inductionQuestionSet.test.ts
@@ -13,6 +13,7 @@ import HasWorkedBeforeValue from '../../../../../enums/hasWorkedBeforeValue'
 import formatAbilityToWorkConstraintFilter from '../../../../../filters/formatAbilityToWorkConstraintFilter'
 import WorkAndInterestsView from '../../../../../routes/overview/workAndInterestsView'
 import formatJobTypeFilter from '../../../../../filters/formatJobTypeFilter'
+import previousWorkExperienceObjectsSortedInScreenOrderFilter from '../../../../../filters/previousWorkExperienceObjectsSortedInScreenOrderFilter'
 
 const njkEnv = nunjucks.configure([
   'node_modules/govuk-frontend/dist/',
@@ -28,6 +29,10 @@ njkEnv.addFilter('sortedAlphabeticallyWithOtherLast', sortedAlphabeticallyWithOt
 njkEnv.addFilter('objectsSortedAlphabeticallyWithOtherLastBy', objectsSortedAlphabeticallyWithOtherLastByFilter)
 njkEnv.addFilter('formatAbilityToWorkConstraint', formatAbilityToWorkConstraintFilter)
 njkEnv.addGlobal('featureToggles', config.featureToggles)
+njkEnv.addFilter(
+  'previousWorkExperienceObjectsSortedInScreenOrder',
+  previousWorkExperienceObjectsSortedInScreenOrderFilter,
+)
 
 describe('Tests for _inductionQuestionSet.njk partial', () => {
   it('Should display last updated using work response if they are not interested in working', () => {
@@ -77,5 +82,60 @@ describe('Tests for _inductionQuestionSet.njk partial', () => {
     const $ = cheerio.load(content)
     const lastUpdated = $('[data-qa=work-interests-last-updated]').first().text().trim()
     expect(lastUpdated).toEqual('Last updated: 20 June 2024 by Future User')
+  })
+
+  it('Should handle has worked before and work experience if previously answered no to wanting to work (back compat)', () => {
+    const anInductionDto = aValidInductionDto({
+      hopingToGetWork: HopingToGetWorkValue.NO,
+    })
+    const content = nunjucks.render(
+      '_inductionQuestionSet.njk',
+      new WorkAndInterestsView(aValidPrisonerSummary(), {
+        problemRetrievingData: false,
+        inductionDto: {
+          ...anInductionDto,
+          previousWorkExperiences: undefined,
+          workOnRelease: {
+            ...anInductionDto.workOnRelease,
+            updatedAt: new Date('2021-05-10T10:00:00Z'),
+            updatedByDisplayName: 'Some User',
+          },
+        },
+      }),
+    )
+    const $ = cheerio.load(content)
+    const lastUpdated = $('[data-qa=work-experience-last-updated]').first().text().trim()
+    expect(lastUpdated).toEqual('Last updated: 10 May 2021 by Some User')
+    const hasWorkedBeforeAnswer = $('[data-qa=has-worked-before-answer]').first().text().trim()
+    expect(hasWorkedBeforeAnswer).toEqual('Not entered.')
+    const hasWorkedBeforeLink = $('[data-qa=has-worked-before-change-link]').first().text().trim()
+    expect(hasWorkedBeforeLink).toEqual('Add worked before')
+  })
+
+  it('Should handle has worked before and work experience if wanting to work', () => {
+    const anInductionDto = aValidInductionDto({
+      hopingToGetWork: HopingToGetWorkValue.YES,
+    })
+    const content = nunjucks.render(
+      '_inductionQuestionSet.njk',
+      new WorkAndInterestsView(aValidPrisonerSummary(), {
+        problemRetrievingData: false,
+        inductionDto: {
+          ...anInductionDto,
+          previousWorkExperiences: {
+            ...anInductionDto.previousWorkExperiences,
+            updatedByDisplayName: 'Worked Before User',
+            updatedAt: new Date('2024-06-20T10:00:00Z'),
+          },
+        },
+      }),
+    )
+    const $ = cheerio.load(content)
+    const lastUpdated = $('[data-qa=work-experience-last-updated]').first().text().trim()
+    expect(lastUpdated).toEqual('Last updated: 20 June 2024 by Worked Before User')
+    const hasWorkedBeforeAnswer = $('[data-qa=has-worked-before-answer]').first().text().trim()
+    expect(hasWorkedBeforeAnswer).toEqual('Yes')
+    const hasWorkedBeforeLink = $('[data-qa=has-worked-before-change-link]').first().text().trim()
+    expect(hasWorkedBeforeLink).toEqual('Change worked before')
   })
 })
