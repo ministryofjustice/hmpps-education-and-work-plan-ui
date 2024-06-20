3ae46d97e9bf73940eadf3e33c4bb3ba3783d67e
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-06-20 10:33:31 +0100

    RR-861 - Fixed bug with personal skills and interests not displaying correctly

---
+++ server/views/pages/overview/partials/workAndInterestsTab/_personalSkillsAndInterestsSummaryCard.test.ts
@@ -0,0 +1,102 @@
+import nunjucks from 'nunjucks'
+import * as cheerio from 'cheerio'
+import aValidInductionDto from '../../../../../testsupport/inductionDtoTestDataBuilder'
+import objectsSortedAlphabeticallyWithOtherLastByFilter from '../../../../../filters/objectsSortedAlphabeticallyWithOtherLastByFilter'
+import formatSkillFilter from '../../../../../filters/formatSkillFilter'
+import formatPersonalInterestFilter from '../../../../../filters/formatPersonalInterestFilter'
+import formatDateFilter from '../../../../../filters/formatDateFilter'
+
+const njkEnv = nunjucks.configure([
+  'node_modules/govuk-frontend/dist/',
+  'node_modules/@ministryofjustice/frontend/',
+  'server/views/',
+  __dirname,
+])
+njkEnv
+  .addFilter('objectsSortedAlphabeticallyWithOtherLastBy', objectsSortedAlphabeticallyWithOtherLastByFilter)
+  .addFilter('formatDate', formatDateFilter)
+  .addFilter('formatSkill', formatSkillFilter)
+  .addFilter('formatPersonalInterest', formatPersonalInterestFilter)
+
+describe('_personalSkillsAndInterestsSummaryCard', () => {
+  it('should display Skills and Interests given induction with personal skills and interests', () => {
+    // Given
+    const inductionDto = aValidInductionDto()
+    const context = { induction: { inductionDto } }
+
+    // When
+    const content = nunjucks.render('_personalSkillsAndInterestsSummaryCard.njk', context)
+    const $ = cheerio.load(content)
+
+    // Then
+    const skills = $('[data-qa=skills]')
+      .find('li')
+      .toArray()
+      .map(el => $(el).text().trim())
+    expect(skills).toEqual(['Teamwork', 'Willingness to learn', 'Other - Tenacity'])
+    expect($('[data-qa=skills-not-recorded]').length).toEqual(0)
+    expect($('[data-qa=skills-change-link]').text().trim()).toEqual('Change skills')
+
+    const personalInterests = $('[data-qa=personal-interests]')
+      .find('li')
+      .toArray()
+      .map(el => $(el).text().trim())
+    expect(personalInterests).toEqual(['Creative', 'Digital', 'Other - Renewable energy'])
+    expect($('[data-qa=personal-interests-not-recorded]').length).toEqual(0)
+    expect($('[data-qa=personal-interests-change-link]').text().trim()).toEqual('Change personal interests')
+    expect($('[data-qa=last-updated]').text().trim()).toEqual('Last updated: 19 June 2023 by Alex Smith')
+  })
+
+  it('should display Add link for Skills and Interests given personal skills and interests are undefined', () => {
+    // Given
+    const inductionDto = aValidInductionDto()
+    inductionDto.personalSkillsAndInterests = undefined
+    const context = { induction: { inductionDto } }
+
+    // When
+    const content = nunjucks.render('_personalSkillsAndInterestsSummaryCard.njk', context)
+    const $ = cheerio.load(content)
+
+    // Then
+    expect($('[data-qa=skills]').length).toEqual(0)
+    expect($('[data-qa=skills-not-recorded]').length).toEqual(1)
+    expect($('[data-qa=skills-change-link]').text().trim()).toEqual('Add skills')
+
+    expect($('[data-qa=personal-interests]').length).toEqual(0)
+    expect($('[data-qa=personal-interests-not-recorded]').length).toEqual(1)
+    expect($('[data-qa=personal-interests-change-link]').text().trim()).toEqual('Add personal interests')
+    expect($('[data-qa=last-updated]').length).toEqual(0)
+  })
+
+  it('should display Add link for Skills given empty array of personal skills', () => {
+    // Given
+    const inductionDto = aValidInductionDto()
+    inductionDto.personalSkillsAndInterests.skills = []
+    const context = { induction: { inductionDto } }
+
+    // When
+    const content = nunjucks.render('_personalSkillsAndInterestsSummaryCard.njk', context)
+    const $ = cheerio.load(content)
+
+    // Then
+    expect($('[data-qa=skills]').length).toEqual(0)
+    expect($('[data-qa=skills-not-recorded]')).not.toBeNull()
+    expect($('[data-qa=skills-change-link]').text().trim()).toEqual('Add skills')
+  })
+
+  it('should display Add link for Interests given empty array of personal interests', () => {
+    // Given
+    const inductionDto = aValidInductionDto()
+    inductionDto.personalSkillsAndInterests.interests = []
+    const context = { induction: { inductionDto } }
+
+    // When
+    const content = nunjucks.render('_personalSkillsAndInterestsSummaryCard.njk', context)
+    const $ = cheerio.load(content)
+
+    // Then
+    expect($('[data-qa=personal-interests]').length).toEqual(0)
+    expect($('[data-qa=personal-interests-not-recorded]')).not.toBeNull()
+    expect($('[data-qa=personal-interests-change-link]').text().trim()).toEqual('Add personal interests')
+  })
+})
