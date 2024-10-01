90be3e0ee1853e4faffde5e1c2feb60b8ee13b4e
lynnette-orr <lynnette.orr@digital.justice.gov.uk>
2024-10-01 12:12:36 +0100

    Add controller and view (#826)
    * Add controller and view
    
    * Pr comments, unit test
    
    * Lint fix

---
+++ server/routes/overview/overviewView.ts
@@ -0,0 +1,70 @@
+import type { FunctionalSkills, InPrisonCourseRecords, PrisonerSummary } from 'viewModels'
+import dateComparator from '../dateComparator'
+
+type RenderArgs = {
+  tab: string
+  prisonerSummary: PrisonerSummary
+  functionalSkills: FunctionalSkills
+  inPrisonCourses: InPrisonCourseRecords
+  isPostInduction: boolean
+  lastUpdatedBy: string | null
+  lastUpdatedDate: Date | null
+  lastUpdatedAtPrisonName: string | null
+  noGoals: boolean
+  goalCounts: { activeCount: number; archivedCount: number; completedCount: number }
+  hasWithdrawnOrInProgressCourses: boolean
+  hasCoursesCompletedMoreThan12MonthsAgo: boolean
+}
+
+export default class OverviewView {
+  constructor(
+    private readonly prisonerSummary: PrisonerSummary,
+    private readonly functionalSkills: FunctionalSkills,
+    private readonly inPrisonCourses: InPrisonCourseRecords,
+    private readonly isPostInduction: boolean,
+    private readonly goalData: {
+      lastUpdatedBy: string | null
+      lastUpdatedDate: Date | null
+      lastUpdatedAtPrisonName: string | null
+      noGoals: boolean
+      goalCounts: { activeCount: number; archivedCount: number; completedCount: number }
+    },
+  ) {}
+
+  get renderArgs(): RenderArgs {
+    const completedCourses = [...(this.inPrisonCourses.coursesByStatus?.COMPLETED || [])].sort((left, right) =>
+      dateComparator(left.courseCompletionDate, right.courseCompletionDate, 'DESC'),
+    )
+
+    const inProgressCourses = [...(this.inPrisonCourses.coursesByStatus?.IN_PROGRESS || [])].sort((left, right) =>
+      dateComparator(left.courseStartDate, right.courseStartDate, 'DESC'),
+    )
+
+    const withdrawnCourses = [
+      ...(this.inPrisonCourses.coursesByStatus?.WITHDRAWN || []).concat(
+        this.inPrisonCourses.coursesByStatus?.TEMPORARILY_WITHDRAWN || [],
+      ),
+    ].sort((left, right) => dateComparator(left.courseStartDate, right.courseStartDate, 'DESC'))
+
+    const hasCoursesCompletedMoreThan12MonthsAgo = completedCourses.some(inPrisonCourse => {
+      const twelveMonthsAgo = new Date()
+      twelveMonthsAgo.setMonth(twelveMonthsAgo.getMonth() - 12)
+      return inPrisonCourse.courseCompletionDate < twelveMonthsAgo
+    })
+
+    return {
+      tab: 'overview',
+      prisonerSummary: this.prisonerSummary,
+      functionalSkills: this.functionalSkills,
+      inPrisonCourses: this.inPrisonCourses,
+      isPostInduction: this.isPostInduction,
+      lastUpdatedBy: this.goalData.lastUpdatedBy,
+      lastUpdatedDate: this.goalData.lastUpdatedDate,
+      lastUpdatedAtPrisonName: this.goalData.lastUpdatedAtPrisonName,
+      noGoals: this.goalData.noGoals,
+      goalCounts: this.goalData.goalCounts,
+      hasWithdrawnOrInProgressCourses: inProgressCourses.length > 0 || withdrawnCourses.length > 0,
+      hasCoursesCompletedMoreThan12MonthsAgo,
+    }
+  }
+}
