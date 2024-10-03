7863cfee9e42e43cc12e1223b2a7169e75be8d0a
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2024-10-03 10:56:07 +0100

    RR-964 - Feature toggle and basic routes for the Plan Review journey (#832)

--- server/config.ts
+++ server/config.ts
@@ -179,5 +179,6 @@ export default {
     // someToggleEnabled: toBoolean(get('SOME_TOGGLE_ENABLED', false)),
     qualificationsEnabled: toBoolean(get('QUALIFICATIONS_ENABLED', false)),
     newOverviewPageEnabled: toBoolean(get('NEW_OVERVIEW_PAGE_ENABLED', false)),
+    reviewsPrisonsEnabled: get('REVIEWS_PRISONS_ENABLED', ''),
   },
 }
