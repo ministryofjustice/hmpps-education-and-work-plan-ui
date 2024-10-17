3dde95cfb53ea039341ad107ce344323326e9fc8
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-10-17 09:54:48 +0100

    RR-1006 / RR-1007 - Correcting the order of archived and in-progress goals

--- server/views/pages/overview/partials/goalsTab/goalsTabContents.test.ts
+++ server/views/pages/overview/partials/goalsTab/goalsTabContents.test.ts
@@ -1,11 +1,11 @@
 import nunjucks from 'nunjucks'
 import * as cheerio from 'cheerio'
+import { startOfDay, toDate } from 'date-fns'
 import aValidPrisonerSummary from '../../../../../testsupport/prisonerSummaryTestDataBuilder'
-import { aValidGoalResponse } from '../../../../../testsupport/actionPlanResponseTestDataBuilder'
-import GoalStatusValue from '../../../../../enums/goalStatusValue'
 import formatDateFilter from '../../../../../filters/formatDateFilter'
 import formatStepStatusValueFilter from '../../../../../filters/formatStepStatusValueFilter'
 import formatReasonToArchiveGoalFilter from '../../../../../filters/formatReasonToArchiveGoalFilter'
+import { aValidGoal } from '../../../../../testsupport/actionPlanTestDataBuilder'
 
 const njkEnv = nunjucks.configure([
   'node_modules/govuk-frontend/govuk/',
@@ -22,13 +22,7 @@ njkEnv.addFilter('formatStepStatusValue', formatStepStatusValueFilter)
 njkEnv.addFilter('formatReasonToArchiveGoal', formatReasonToArchiveGoalFilter)
 
 const prisonerSummary = aValidPrisonerSummary()
-const inProgressGoal = { ...aValidGoalResponse(), status: GoalStatusValue.ACTIVE, steps: [{ title: 'Learn French' }] }
-const archivedGoal = {
-  ...aValidGoalResponse(),
-  status: GoalStatusValue.ARCHIVED,
-  steps: [{ title: 'Learn woodwork' }],
-  archiveReason: 'PRISONER_NO_LONGER_WANTS_TO_WORK_TOWARDS_GOAL',
-}
+
 const template = 'goalsTabContents.njk'
 
 describe('ViewGoalsController', () => {
@@ -36,11 +30,27 @@ describe('ViewGoalsController', () => {
     jest.clearAllMocks()
   })
 
-  it('should render in-progress goals correctly', async () => {
+  it('should render in-progress goals correctly, in order of target completion date, soonest first', async () => {
     // Given
+    const inProgressGoal1 = aValidGoal({
+      targetCompletionDate: startOfDay('2024-12-01'),
+      status: 'ACTIVE',
+      title: 'Learn French',
+    })
+    const inProgressGoal2 = aValidGoal({
+      targetCompletionDate: startOfDay('2024-11-01'),
+      status: 'ACTIVE',
+      title: 'Learn Spanish',
+    })
+    const inProgressGoal3 = aValidGoal({
+      targetCompletionDate: startOfDay('2025-01-01'),
+      status: 'ACTIVE',
+      title: 'Learn German',
+    })
+
     const pageViewModel = {
       prisonerSummary,
-      inProgressGoals: [inProgressGoal],
+      inProgressGoals: [inProgressGoal1, inProgressGoal2, inProgressGoal3],
       problemRetrievingData: false,
       tab: 'goals',
     }
@@ -50,18 +60,58 @@ describe('ViewGoalsController', () => {
     const $ = cheerio.load(content)
 
     // Then
-    expect($('[data-qa="in-progress-goal-summary-card"]').length).toEqual(1)
-    expect($(`[data-qa="goal-${inProgressGoal.goalReference}-update-button"]`).length).toEqual(1)
-    expect($('[data-qa="in-progress-goal-summary-card"]').first().text()).toContain('Learn French')
-    const hint = $('[data-qa=goal-last-updated-hint]').first()
-    expect(hint.text().trim()).toEqual('Last updated on 23 September 2023 by Alex Smith')
+    expect($('[data-qa="in-progress-goal-summary-card"] [data-qa="goal-summary-card-heading"]').length).toEqual(3)
+    // Assert the goals are in the correct order, based on target completion date, soonest first
+    // First rendered goal ....
+    expect(
+      $('[data-qa="in-progress-goal-summary-card"] [data-qa="goal-summary-card-heading"]').eq(0).text().trim(),
+    ).toEqual('Achieve goal by 1 November 2024')
+    expect(
+      $('[data-qa="in-progress-goal-summary-card"] [data-qa="goal-summary-card-goal-title"]').eq(0).text().trim(),
+    ).toEqual('Learn Spanish')
+    // Second rendered goal ....
+    expect(
+      $('[data-qa="in-progress-goal-summary-card"] [data-qa="goal-summary-card-heading"]').eq(1).text().trim(),
+    ).toEqual('Achieve goal by 1 December 2024')
+    expect(
+      $('[data-qa="in-progress-goal-summary-card"] [data-qa="goal-summary-card-goal-title"]').eq(1).text().trim(),
+    ).toEqual('Learn French')
+    // Third rendered goal ....
+    expect(
+      $('[data-qa="in-progress-goal-summary-card"] [data-qa="goal-summary-card-heading"]').eq(2).text().trim(),
+    ).toEqual('Achieve goal by 1 January 2025')
+    expect(
+      $('[data-qa="in-progress-goal-summary-card"] [data-qa="goal-summary-card-goal-title"]').eq(2).text().trim(),
+    ).toEqual('Learn German')
+    // Assert the first goal's hint text - if this one is correctly formatted/laid out, they all will be
+    const hint = $('[data-qa="in-progress-goal-summary-card"] [data-qa=goal-last-updated-hint]').first()
+    expect(hint.text().trim()).toEqual('Last updated on 23 September 2023 by Alex Smith, Brixton (HMP)')
   })
 
-  it('should render archived goals correctly', async () => {
+  it('should render archived goals correctly, in order or archival date (updatedDate), soonest last', async () => {
     // Given
+    const archivedGoal1 = aValidGoal({
+      updatedAt: toDate('2024-12-01T09:12:23.123Z'),
+      targetCompletionDate: startOfDay('2024-12-31'),
+      status: 'ARCHIVED',
+      title: 'Learn French',
+    })
+    const archivedGoal2 = aValidGoal({
+      updatedAt: toDate('2024-12-01T08:57:18.561Z'),
+      targetCompletionDate: startOfDay('2024-12-31'),
+      status: 'ARCHIVED',
+      title: 'Learn Spanish',
+    })
+    const archivedGoal3 = aValidGoal({
+      updatedAt: toDate('2025-01-01T14:43:09.931Z'),
+      targetCompletionDate: startOfDay('2025-06-30'),
+      status: 'ARCHIVED',
+      title: 'Learn German',
+    })
+
     const pageViewModel = {
       prisonerSummary,
-      archivedGoals: [archivedGoal],
+      archivedGoals: [archivedGoal1, archivedGoal2, archivedGoal3],
       problemRetrievingData: false,
       tab: 'goals',
     }
@@ -71,12 +121,31 @@ describe('ViewGoalsController', () => {
     const $ = cheerio.load(content)
 
     // Then
-    expect($('[data-qa="archived-goal-summary-card"]').length).toEqual(1)
-    expect($(`[data-qa="goal-${archivedGoal.goalReference}-unarchive-button"]`).length).toEqual(1)
-    expect($('[data-qa="archived-goal-summary-card"]').first().text()).toContain('Learn woodwork')
-    const lastUpdatedHint = $('[data-qa=goal-last-updated-hint]').first()
-    expect(lastUpdatedHint.text().trim()).toEqual('Archived on 23 September 2023 by Alex Smith')
-    const reasonHint = $('[data-qa=goal-archive-reason-hint]').first()
-    expect(reasonHint.text().trim()).toEqual('Reason: Prisoner no longer wants to work towards this goal')
+    expect($('[data-qa="archived-goal-summary-card"] [data-qa="goal-summary-card-heading"]').length).toEqual(3)
+    // Assert the goals are in the correct order, based on updated date, soonest last
+    // First rendered goal ....
+    expect(
+      $('[data-qa="archived-goal-summary-card"] [data-qa="goal-summary-card-heading"]').eq(0).text().trim(),
+    ).toEqual('Achieve goal by 30 June 2025')
+    expect(
+      $('[data-qa="archived-goal-summary-card"] [data-qa="goal-summary-card-goal-title"]').eq(0).text().trim(),
+    ).toEqual('Learn German')
+    // Second rendered goal ....
+    expect(
+      $('[data-qa="archived-goal-summary-card"] [data-qa="goal-summary-card-heading"]').eq(1).text().trim(),
+    ).toEqual('Achieve goal by 31 December 2024')
+    expect(
+      $('[data-qa="archived-goal-summary-card"] [data-qa="goal-summary-card-goal-title"]').eq(1).text().trim(),
+    ).toEqual('Learn French')
+    // Third rendered goal ....
+    expect(
+      $('[data-qa="archived-goal-summary-card"] [data-qa="goal-summary-card-heading"]').eq(2).text().trim(),
+    ).toEqual('Achieve goal by 31 December 2024')
+    expect(
+      $('[data-qa="archived-goal-summary-card"] [data-qa="goal-summary-card-goal-title"]').eq(2).text().trim(),
+    ).toEqual('Learn Spanish')
+    // Assert the first goal's hint text - if this one is correctly formatted/laid out, they all will be
+    const hint = $('[data-qa="archived-goal-summary-card"] [data-qa=goal-last-updated-hint]').first()
+    expect(hint.text().trim()).toEqual('Archived on 1 January 2025 by Alex Smith, Brixton (HMP)')
   })
 })
