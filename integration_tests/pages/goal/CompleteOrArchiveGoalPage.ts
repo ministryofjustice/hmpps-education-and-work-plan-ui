73f29c9081bbc79fef5f412a0bb0fb3204721af1
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-10-18 14:55:33 +0100

    integration test part one: fix archive goal tests

---
+++ integration_tests/pages/goal/CompleteOrArchiveGoalPage.ts
@@ -0,0 +1,18 @@
+import Page, { PageElement } from '../page'
+import ReasonToArchiveGoalValue from '../../../server/enums/ReasonToArchiveGoalValue'
+import CompleteOrArchiveGoalValue from '../../../server/enums/CompleteOrArchiveGoalValue'
+
+export default class CompleteOrArchiveGoalPage extends Page {
+  constructor() {
+    super('complete-or-archive-goal')
+  }
+
+  selectOption(value: CompleteOrArchiveGoalValue): CompleteOrArchiveGoalPage {
+    this.radio(value).click()
+    return this
+  }
+
+  private goalReferenceInputValue = (): PageElement => cy.get('[data-qa=goal-reference]')
+
+  private radio = (value: string): PageElement => cy.get(`.govuk-radios__input[value='${value}']`)
+}
