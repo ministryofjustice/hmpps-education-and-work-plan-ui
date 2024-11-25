d5c67c445222d4bda5525016976500de134fe8e9
Lynnette Orr <lynnette.orr@digital.justice.gov.uk>
2024-11-25 11:43:30 +0000

    Refactor routes

--- integration_tests/e2e/reviewPlan/exemption/exemptionRecorded.cy.ts
+++ integration_tests/e2e/reviewPlan/exemption/exemptionRecorded.cy.ts
@@ -2,35 +2,38 @@
  * Cypress tests that test the 'Exemption recorded' page
  */
 
-import ReviewPlanExemptionReasonValue from '../../../../server/enums/reviewPlanExemptionReasonValue'
 import OverviewPage from '../../../pages/overview/OverviewPage'
 import Page from '../../../pages/page'
 import ConfirmExemptionPage from '../../../pages/reviewPlan/exemption/confirmExemptionPage'
-import ExemptionReasonPage from '../../../pages/reviewPlan/exemption/exemptionPage'
 import ExemptionRecordedPage from '../../../pages/reviewPlan/exemption/exemptionRecordedPage'
 
 context(`Review exemption recorded page`, () => {
   const prisonNumber = 'G6115VJ'
+  beforeEach(() => {
+    cy.task('stubSignInAsUserWithEditAuthority')
+  })
 
   it(`Should navigate to the 'Overview' page when 'Continue' button is clicked`, () => {
     // Given
-    cy.task('stubSignInAsUserWithEditAuthority')
     cy.signIn()
-    cy.visit(`/plan/${prisonNumber}/review/exemption`)
+    cy.createExemption()
 
     // When
-    Page.verifyOnPage(ExemptionReasonPage) //
-      .selectExemptionReason(ReviewPlanExemptionReasonValue.EXEMPT_PRISONER_DRUG_OR_ALCOHOL_DEPENDENCY)
-      .enterExemptionReasonDetails(
-        ReviewPlanExemptionReasonValue.EXEMPT_PRISONER_DRUG_OR_ALCOHOL_DEPENDENCY,
-        'In treatment',
-      )
-      .submitPage()
     Page.verifyOnPage(ConfirmExemptionPage).submitPage()
+    Page.verifyOnPage(ExemptionRecordedPage).submitPage()
 
     // Then
-    Page.verifyOnPage(ExemptionRecordedPage).submitPage()
-    Page.verifyOnPage(OverviewPage) //
-      .hasSuccessMessage('Exemption recorded')
+    Page.verifyOnPage(OverviewPage)
+  })
+
+  it(`should redirect to the 'Overview' page given user tries to navigate directly to 'Review exemption recorded' page - ie. navigate out of sequence`, () => {
+    // Given
+    cy.signIn()
+
+    // When
+    cy.visit(`/plan/${prisonNumber}/review/exemption/recorded`)
+
+    // Then
+    Page.verifyOnPage(OverviewPage)
   })
 })
