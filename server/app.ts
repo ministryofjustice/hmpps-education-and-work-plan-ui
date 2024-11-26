37c5ce273ad8f411e5f381045ab3b2401f04d909
Lynnette Orr <lynnette.orr@digital.justice.gov.uk>
2024-11-26 11:08:26 +0000

    PR comments

--- server/app.ts
+++ server/app.ts
@@ -51,7 +51,7 @@ export default function createApp(services: Services): express.Application {
   app.use(errorMessageMiddleware)
 
   app.get('*', async (req, res, next) => {
-    const user = res.locals.user || {}
+    const { user } = res.locals
     const { activeCaseLoadId } = user.activeCaseLoadId
     res.locals.reviewJourneyEnabledForPrison = config.featureToggles.reviewJourneyEnabledForPrison(activeCaseLoadId)
     next()
