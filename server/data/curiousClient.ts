b37eab667a3b7baa3fbb7587d7e0636b8b81739e
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-09-09 11:45:59 +0100

    RR-1884 - Add Curious client method to get All Assessments for a prisoner (#1247)

--- server/data/curiousClient.ts
+++ server/data/curiousClient.ts
@@ -1,5 +1,5 @@
 import type { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
-import type { LearnerEducationPagedResponse, LearnerProfile } from 'curiousApiClient'
+import type { AllAssessmentDTO, LearnerEducationPagedResponse, LearnerProfile } from 'curiousApiClient'
 import { asSystem, RestClient } from '@ministryofjustice/hmpps-rest-client'
 import restClientErrorHandler from './restClientErrorHandler'
 import config from '../config'
@@ -10,6 +10,9 @@ export default class CuriousClient extends RestClient {
     super('Curious API Client', config.apis.curious, logger, authenticationClient)
   }
 
+  /**
+   * @deprecated - this method calls a Curious 1 endpoint. Use a method that calls a suitable Curious 2 endpoint instead
+   */
   async getLearnerProfile(prisonNumber: string): Promise<Array<LearnerProfile>> {
     return this.get<Array<LearnerProfile>>(
       {
@@ -20,6 +23,9 @@ export default class CuriousClient extends RestClient {
     )
   }
 
+  /**
+   * @deprecated - this method calls a Curious 1 endpoint. Use a method that calls a suitable Curious 2 endpoint instead
+   */
   async getLearnerEducationPage(prisonNumber: string, page = 0): Promise<LearnerEducationPagedResponse> {
     return this.get<LearnerEducationPagedResponse>(
       {
@@ -27,6 +33,24 @@ export default class CuriousClient extends RestClient {
         query: {
           page,
         },
+        errorHandler: restClientErrorHandler({ ignore404: true }),
+      },
+      asSystem('CURIOUS_API'),
+    )
+  }
+
+  /**
+   * Calls the Curious V2 endpoint to get all assessments for a given prisoner
+   * The returned data includes LDD and Functional Skills data as recorded in Curious 1, and all assessments (ALN,
+   * Functional Skills (Maths, English and Digital Skills), ESOL and Reading) as recorded in Curious 2.
+   *
+   * @param prisonNumber
+   * @return AllAssessmentDTO
+   */
+  async getAssessmentsByPrisonNumber(prisonNumber: string): Promise<AllAssessmentDTO> {
+    return this.get<AllAssessmentDTO>(
+      {
+        path: `/learnerAssessments/v2/${prisonNumber}`,
         errorHandler: restClientErrorHandler({ ignore404: true }),
       },
       asSystem('CURIOUS_API'),
