ffbf9255bc329e457bfc00de406f008b5063ec9b
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-09-19 13:18:42 +0100

    RR-1849 - Wire in Curious 2 assessment mapper (#1281)

--- server/routes/overview/mappers/functionalSkillsMapper.test.ts
+++ server/routes/overview/mappers/functionalSkillsMapper.test.ts
@@ -2,11 +2,19 @@ import { parseISO, startOfDay } from 'date-fns'
 import type { LearnerProfile } from 'curiousApiClient'
 import toFunctionalSkills from './functionalSkillsMapper'
 import {
+  aDigitalFunctionalSkillsLearnerAssessmentsDTO,
   aLearnerAssessmentV1DTO,
+  aLearnerAssessmentV2DTO,
   aLearnerLatestAssessmentV1DTO,
+  aMathsFunctionalSkillsLearnerAssessmentsDTO,
   anAllAssessmentDTO,
+  anEnglishFunctionalSkillsLearnerAssessmentsDTO,
+  anEsolLearnerAssessmentsDTO,
+  anExternalAssessmentsDTO,
+  aReadingLearnerAssessmentsDTO,
 } from '../../../testsupport/curiousAssessmentsTestDataBuilder'
-import { aValidCurious1Assessment } from '../../../testsupport/assessmentTestDataBuilder'
+import { aValidCurious1Assessment, aValidCurious2Assessment } from '../../../testsupport/assessmentTestDataBuilder'
+import AssessmentTypeValue from '../../../enums/assessmentTypeValue'
 
 describe('functionalSkillsMapper', () => {
   describe('Map to functional skills from the Curious 1 assessments data as precedence over the Curious 2 assessments', () => {
@@ -31,6 +39,7 @@ describe('functionalSkillsMapper', () => {
       const allAssessments = anAllAssessmentDTO({
         v1Assessments: [
           aLearnerLatestAssessmentV1DTO({
+            // We do not expect this assessment to be mapped because there is an assessment in the array of LearnerProfile
             qualifications: [
               aLearnerAssessmentV1DTO({
                 prisonId: 'MDI',
@@ -41,10 +50,71 @@ describe('functionalSkillsMapper', () => {
             ],
           }),
         ],
+        v2Assessments: aLearnerAssessmentV2DTO({
+          assessments: anExternalAssessmentsDTO({
+            // We expect all of these Curious 2 assessments to be mapped
+            englishFunctionalSkills: [anEnglishFunctionalSkillsLearnerAssessmentsDTO()],
+            mathsFunctionalSkills: [aMathsFunctionalSkillsLearnerAssessmentsDTO()],
+            digitalFunctionalSkillsAssessments: [aDigitalFunctionalSkillsLearnerAssessmentsDTO()],
+            esolAssessments: [anEsolLearnerAssessmentsDTO()],
+            readingAssessments: [aReadingLearnerAssessmentsDTO()],
+          }),
+        }),
       })
 
       const expected = {
         assessments: [
+          // The Curious 2 English assessment
+          aValidCurious2Assessment({
+            type: AssessmentTypeValue.ENGLISH,
+            assessmentDate: startOfDay('2025-10-01'),
+            prisonId: 'MDI',
+            level: 'Entry Level 2',
+            levelBanding: '2.1',
+            nextStep: 'Progress to course at level consistent with assessment result',
+            referral: 'Education Specialist',
+          }),
+          // The Curious 2 Maths assessment
+          aValidCurious2Assessment({
+            type: AssessmentTypeValue.MATHS,
+            assessmentDate: startOfDay('2025-10-01'),
+            prisonId: 'MDI',
+            level: 'Entry Level 2',
+            levelBanding: '2.1',
+            nextStep: 'Progress to course at level consistent with assessment result',
+            referral: 'Education Specialist',
+          }),
+          // The Curious 2 Digital Literacy assessment
+          aValidCurious2Assessment({
+            type: AssessmentTypeValue.DIGITAL_LITERACY,
+            assessmentDate: startOfDay('2025-10-01'),
+            prisonId: 'MDI',
+            level: 'Level 1',
+            levelBanding: '1.2',
+            nextStep: null,
+            referral: null,
+          }),
+          // The Curious 2 Reading assessment
+          aValidCurious2Assessment({
+            type: AssessmentTypeValue.READING,
+            assessmentDate: startOfDay('2025-10-01'),
+            prisonId: 'MDI',
+            level: 'non-reader',
+            levelBanding: null,
+            nextStep: 'Refer for reading support level.',
+            referral: 'Education Specialist',
+          }),
+          // The Curious 2 ESOL assessment
+          aValidCurious2Assessment({
+            type: AssessmentTypeValue.ESOL,
+            assessmentDate: startOfDay('2025-10-01'),
+            prisonId: 'MDI',
+            level: 'ESOL Pathway',
+            levelBanding: null,
+            nextStep: 'English Language Support Level 1',
+            referral: 'Education Specialist',
+          }),
+          // The Curious 1 assessment mapped from the LearnerProfile array
           aValidCurious1Assessment({
             assessmentDate: startOfDay(parseISO('2022-08-29')),
             level: 'Level 3',
@@ -80,6 +150,15 @@ describe('functionalSkillsMapper', () => {
             ],
           }),
         ],
+        v2Assessments: aLearnerAssessmentV2DTO({
+          assessments: anExternalAssessmentsDTO({
+            englishFunctionalSkills: [],
+            mathsFunctionalSkills: [],
+            digitalFunctionalSkillsAssessments: [],
+            esolAssessments: [],
+            readingAssessments: [],
+          }),
+        }),
       })
 
       const expected = {
