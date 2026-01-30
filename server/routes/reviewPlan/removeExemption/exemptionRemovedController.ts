cdb2ef5151fe176a549af218c2625adb2f232fe1
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2026-01-30 10:18:21 +0000

    RR-2174 - Improve error handling for removing review exemption

--- server/routes/reviewPlan/removeExemption/exemptionRemovedController.ts
+++ server/routes/reviewPlan/removeExemption/exemptionRemovedController.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
-import ExemptionRemovedView from './exemptionRemovedView'
 
 export default class ExemptionRemovedController {
   getExemptionRemovedView: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { prisonerSummary, actionPlanReviews } = res.locals
 
-    const view = new ExemptionRemovedView(prisonerSummary, actionPlanReviews)
-    return res.render('pages/reviewPlan/removeExemption/exemptionRemoved/index', { ...view.renderArgs })
+    return res.render('pages/reviewPlan/removeExemption/exemptionRemoved/index', {
+      prisonerSummary,
+      nextReviewDate: actionPlanReviews.latestReviewSchedule.reviewDateTo,
+    })
   }
 
   submitExemptionRemoved: RequestHandler = async (req, res): Promise<void> => {
