af9506810fd376146d17c04e544f7290dedd58e9
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2026-01-30 14:42:48 +0000

    RR-2256 - Added permissions for the Employability Skills features (#1448)

--- server/middleware/roleBaseAccessControl.test.ts
+++ server/middleware/roleBaseAccessControl.test.ts
@@ -1,4 +1,4 @@
-import { userWithRoleCan } from './roleBasedAccessControl'
+import { userHasPermissionTo, userWithRoleCan } from './roleBasedAccessControl'
 import DpsRole from '../enums/dpsRole'
 import ApplicationAction from '../enums/applicationAction'
 
@@ -20,6 +20,9 @@ describe('roleBasedAccessControl', () => {
         ApplicationAction.UPDATE_GOALS,
         ApplicationAction.COMPLETE_AND_ARCHIVE_GOALS,
         ApplicationAction.VIEW_SESSION_SUMMARIES,
+        ApplicationAction.VIEW_EMPLOYABILITY_SKILLS,
+        ApplicationAction.ADD_EMPLOYABILITY_SKILLS_RATING,
+        ApplicationAction.VIEW_EMPLOYABILITY_SKILL_RATINGS,
       ]
 
       // When
@@ -35,6 +38,9 @@ describe('roleBasedAccessControl', () => {
         ApplicationAction.UPDATE_INDUCTION,
         ApplicationAction.RECORD_EDUCATION,
         ApplicationAction.UPDATE_EDUCATION,
+        ApplicationAction.VIEW_EMPLOYABILITY_SKILLS,
+        ApplicationAction.ADD_EMPLOYABILITY_SKILLS_RATING,
+        ApplicationAction.VIEW_EMPLOYABILITY_SKILL_RATINGS,
       ]
 
       // When
@@ -46,13 +52,160 @@ describe('roleBasedAccessControl', () => {
 
     it('should return the actions a user with ROLE_MATCH_LEARNER_RECORD_RW can perform', () => {
       // Given
-      const expected = [ApplicationAction.USE_DPS_LEARNER_RECORD_MATCHING_SERVICE]
+      const expected = [
+        ApplicationAction.VIEW_EMPLOYABILITY_SKILLS,
+        ApplicationAction.ADD_EMPLOYABILITY_SKILLS_RATING,
+        ApplicationAction.VIEW_EMPLOYABILITY_SKILL_RATINGS,
+        ApplicationAction.USE_DPS_LEARNER_RECORD_MATCHING_SERVICE,
+      ]
 
       // When
       const actual = userWithRoleCan(DpsRole.ROLE_MATCH_LEARNER_RECORD_RW)
 
       // Then
       expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('userHasPermissionTo', () => {
+    it.each([
+      ApplicationAction.RECORD_INDUCTION,
+      ApplicationAction.EXEMPT_INDUCTION,
+      ApplicationAction.REMOVE_INDUCTION_EXEMPTION,
+      ApplicationAction.UPDATE_INDUCTION,
+      ApplicationAction.RECORD_EDUCATION,
+      ApplicationAction.UPDATE_EDUCATION,
+      ApplicationAction.RECORD_REVIEW,
+      ApplicationAction.EXEMPT_REVIEW,
+      ApplicationAction.REMOVE_REVIEW_EXEMPTION,
+      ApplicationAction.CREATE_GOALS,
+      ApplicationAction.UPDATE_GOALS,
+      ApplicationAction.COMPLETE_AND_ARCHIVE_GOALS,
+      ApplicationAction.VIEW_SESSION_SUMMARIES,
+      ApplicationAction.VIEW_EMPLOYABILITY_SKILLS,
+      ApplicationAction.ADD_EMPLOYABILITY_SKILLS_RATING,
+      ApplicationAction.VIEW_EMPLOYABILITY_SKILL_RATINGS,
+    ])('user with ROLE_LWP_MANAGER has permission to %s', applicationAction => {
+      // Given
+      const userRoles = [DpsRole.ROLE_LWP_MANAGER]
+
+      // When
+      const actual = userHasPermissionTo(applicationAction, userRoles)
+
+      // Then
+      expect(actual).toEqual(true)
+    })
+
+    it.each([ApplicationAction.USE_DPS_LEARNER_RECORD_MATCHING_SERVICE])(
+      'user with ROLE_LWP_MANAGER does not have permission to %s',
+      applicationAction => {
+        // Given
+        const userRoles = [DpsRole.ROLE_LWP_MANAGER]
+
+        // When
+        const actual = userHasPermissionTo(applicationAction, userRoles)
+
+        // Then
+        expect(actual).toEqual(false)
+      },
+    )
+
+    it.each([
+      ApplicationAction.UPDATE_INDUCTION,
+      ApplicationAction.RECORD_EDUCATION,
+      ApplicationAction.UPDATE_EDUCATION,
+      ApplicationAction.VIEW_EMPLOYABILITY_SKILLS,
+      ApplicationAction.ADD_EMPLOYABILITY_SKILLS_RATING,
+      ApplicationAction.VIEW_EMPLOYABILITY_SKILL_RATINGS,
+    ])('user with ROLE_LWP_CONTRIBUTOR has permission to %s', applicationAction => {
+      // Given
+      const userRoles = [DpsRole.ROLE_LWP_CONTRIBUTOR]
+
+      // When
+      const actual = userHasPermissionTo(applicationAction, userRoles)
+
+      // Then
+      expect(actual).toEqual(true)
+    })
+
+    it.each([
+      ApplicationAction.RECORD_INDUCTION,
+      ApplicationAction.EXEMPT_INDUCTION,
+      ApplicationAction.REMOVE_INDUCTION_EXEMPTION,
+      ApplicationAction.RECORD_REVIEW,
+      ApplicationAction.EXEMPT_REVIEW,
+      ApplicationAction.REMOVE_REVIEW_EXEMPTION,
+      ApplicationAction.CREATE_GOALS,
+      ApplicationAction.UPDATE_GOALS,
+      ApplicationAction.COMPLETE_AND_ARCHIVE_GOALS,
+      ApplicationAction.VIEW_SESSION_SUMMARIES,
+      ApplicationAction.USE_DPS_LEARNER_RECORD_MATCHING_SERVICE,
+    ])('user with ROLE_LWP_CONTRIBUTOR does not have permission to %s', applicationAction => {
+      // Given
+      const userRoles = [DpsRole.ROLE_LWP_CONTRIBUTOR]
+
+      // When
+      const actual = userHasPermissionTo(applicationAction, userRoles)
+
+      // Then
+      expect(actual).toEqual(false)
+    })
+
+    it.each([
+      ApplicationAction.VIEW_EMPLOYABILITY_SKILLS,
+      ApplicationAction.ADD_EMPLOYABILITY_SKILLS_RATING,
+      ApplicationAction.VIEW_EMPLOYABILITY_SKILL_RATINGS,
+    ])('user with no LWP roles has permission to %s', applicationAction => {
+      // Given
+      const userRoles: Array<DpsRole> = []
+
+      // When
+      const actual = userHasPermissionTo(applicationAction, userRoles)
+
+      // Then
+      expect(actual).toEqual(true)
+    })
+
+    it.each([
+      ApplicationAction.UPDATE_INDUCTION,
+      ApplicationAction.RECORD_EDUCATION,
+      ApplicationAction.UPDATE_EDUCATION,
+      ApplicationAction.RECORD_INDUCTION,
+      ApplicationAction.EXEMPT_INDUCTION,
+      ApplicationAction.REMOVE_INDUCTION_EXEMPTION,
+      ApplicationAction.RECORD_REVIEW,
+      ApplicationAction.EXEMPT_REVIEW,
+      ApplicationAction.REMOVE_REVIEW_EXEMPTION,
+      ApplicationAction.CREATE_GOALS,
+      ApplicationAction.UPDATE_GOALS,
+      ApplicationAction.COMPLETE_AND_ARCHIVE_GOALS,
+      ApplicationAction.VIEW_SESSION_SUMMARIES,
+      ApplicationAction.USE_DPS_LEARNER_RECORD_MATCHING_SERVICE,
+    ])('user with no LWP roles does not have permission to %s', applicationAction => {
+      // Given
+      const userRoles: Array<DpsRole> = []
+
+      // When
+      const actual = userHasPermissionTo(applicationAction, userRoles)
+
+      // Then
+      expect(actual).toEqual(false)
+    })
+
+    it.each([
+      'ROLE_MATCH_LEARNER_RECORD_RW',
+      'ROLE_LWP_MANAGER, ROLE_MATCH_LEARNER_RECORD_RW',
+      'ROLE_LWP_CONTRIBUTOR, ROLE_MATCH_LEARNER_RECORD_RW',
+      'ROLE_LWP_CONTRIBUTOR, ROLE_LWP_MANAGER, ROLE_MATCH_LEARNER_RECORD_RW',
+    ])('user with roles %s has permission to USE_DPS_LEARNER_RECORD_MATCHING_SERVICE', roles => {
+      // Given
+      const userRoles = roles.split(',').map(role => role.trim() as DpsRole)
+
+      // When
+      const actual = userHasPermissionTo(ApplicationAction.USE_DPS_LEARNER_RECORD_MATCHING_SERVICE, userRoles)
+
+      // Then
+      expect(actual).toEqual(true)
     })
   })
 })
--- server/middleware/roleBasedAccessControl.ts
+++ server/middleware/roleBasedAccessControl.ts
@@ -7,7 +7,7 @@ import DpsRole from '../enums/dpsRole'
  * The list of [ApplicationRole]s should be considered an "or" list. Users require any one of the listed roles for each
  * action.
  */
-const rolesForAction = (): Record<ApplicationAction, Array<DpsRole>> => ({
+const rolesForAction: Record<ApplicationAction, Array<DpsRole>> = {
   [ApplicationAction.RECORD_INDUCTION]: [DpsRole.ROLE_LWP_MANAGER],
   [ApplicationAction.EXEMPT_INDUCTION]: [DpsRole.ROLE_LWP_MANAGER],
   [ApplicationAction.REMOVE_INDUCTION_EXEMPTION]: [DpsRole.ROLE_LWP_MANAGER],
@@ -21,28 +21,32 @@ const rolesForAction = (): Record<ApplicationAction, Array<DpsRole>> => ({
   [ApplicationAction.UPDATE_GOALS]: [DpsRole.ROLE_LWP_MANAGER],
   [ApplicationAction.COMPLETE_AND_ARCHIVE_GOALS]: [DpsRole.ROLE_LWP_MANAGER],
   [ApplicationAction.VIEW_SESSION_SUMMARIES]: [DpsRole.ROLE_LWP_MANAGER],
+  [ApplicationAction.VIEW_EMPLOYABILITY_SKILLS]: [],
+  [ApplicationAction.ADD_EMPLOYABILITY_SKILLS_RATING]: [],
+  [ApplicationAction.VIEW_EMPLOYABILITY_SKILL_RATINGS]: [],
   [ApplicationAction.USE_DPS_LEARNER_RECORD_MATCHING_SERVICE]: [DpsRole.ROLE_MATCH_LEARNER_RECORD_RW],
-})
+}
 
 /**
  * A convenience middleware function that uses [authorisationMiddleware] to grant or deny access to a given
  * [ApplicationAction] based on the user's roles.
  */
-const checkUserHasPermissionTo = (action: ApplicationAction) => authorisationMiddleware(rolesForAction()[action])
+const checkUserHasPermissionTo = (action: ApplicationAction) => authorisationMiddleware(rolesForAction[action])
 
 /**
  * Returns true if the specified [ApplicationAction] can be satisfied with any of the specified user roles.
  */
 const userHasPermissionTo = (action: ApplicationAction, userRoles: string[]): boolean => {
-  return userRoles.some(role => (rolesForAction()[action] || []).includes(role as DpsRole))
+  const requiredRoles = rolesForAction[action] || []
+  return requiredRoles.length === 0 || userRoles.some(role => requiredRoles.includes(role as DpsRole))
 }
 
 /**
  * Helper method to return an array of [ApplicationAction] for a given [DpsRole] that a user might have.
  */
 const userWithRoleCan = (role: DpsRole): Array<ApplicationAction> =>
-  Object.keys(ApplicationAction)
-    .filter(action => rolesForAction()[action as ApplicationAction].includes(role))
-    .map(action => action as ApplicationAction)
+  Object.values(ApplicationAction).filter(
+    action => rolesForAction[action].length === 0 || rolesForAction[action].includes(role),
+  )
 
 export { checkUserHasPermissionTo, userHasPermissionTo, userWithRoleCan }
