5c0309751f96393adff39fb13f8820ed943bc629
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2026-03-25 14:19:38 +0000

    Updated allowed-scripts

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,12 +2,14 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
+    // Needed by esbuild for watching files during development
+    'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     // Needed by cypress
-    'node_modules/cypress@15.11.0': 'ALLOW',
+    'node_modules/cypress@15.13.0': 'ALLOW',
     // Provides native integration, supporting ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.27.3': 'ALLOW',
+    'node_modules/esbuild@0.27.4': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
